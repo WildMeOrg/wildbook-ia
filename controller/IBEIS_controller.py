4def0b63e1694844e320d32b39e39efba6585e1d
Jon Crall <crallj@rpi.edu>
2014-03-19 14:51:22 -0400

    Added some comments

--- controller/IBEIS_controller.py
+++ controller/IBEIS_controller.py
@@ -33,23 +33,31 @@ class IBEISController(object):
     def add_chips(ibs, gid_list, roi_list, theta_list):
         pass
 
-    #----------------
-    # --- Setters ---
-    #----------------
+    #---------------------
+    # --- Chip Setters ---
+    #---------------------
 
     def set_chip_roi(ibs, cid, roi):
+        'returns a list (x, y, w, h) tuples'
         pass
 
-    def set_chip_theta(ibs, cid, theta):
+    def set_chip_thetas(ibs, cid_list, theta_list):
         pass
 
-    def set_chip_name(ibs, cid, name):
+    def set_chip_names(ibs, cid_list, name_list):
         pass
 
-    def set_chip_property(ibs, cid, key):
+    def set_chip_properties(ibs, cid_list, key, val_list):
         pass
 
-    def set_image_property(ibs, gid, key):
+    #----------------------
+    # --- Image Setters ---
+    #----------------------
+
+    def set_image_eid(ibs, gid_list, eid_list):
+        pass
+
+    def set_image_properties(ibs, gid_list, key, val_list):
         pass
 
     #----------------------
@@ -57,57 +65,67 @@ class IBEISController(object):
     #----------------------
 
     def get_images(ibs, gid_list):
-        # return raw pixel data
+        'returns a list of images (numpy arrays)'
         pass
 
     def get_image_paths(ibs, gid_list):
-        # return image paths
+        'returns a list of strings (image paths)'
         pass
 
     def get_image_size(ibs, gid_list):
+        'returns a list (width, height) tuples'
         pass
 
     def get_image_unixtime(hs, gid_list):
         pass
 
-    def get_image_property(ibs, gid_list, key):
+    def get_image_eid(ibs, gid_list):
+        'returns a list of encounter ids'
         pass
 
     def get_cids_in_gids(ibs, gid_list):
+        'returns a list of cid lists: [(1, 2), (3), (), (4, 5, 6) ...]'
         # for each image return chips in that image
         pass
 
     def get_num_cids_in_gids(ibs, gid_list):
         return map(len, ibs.get_cids_in_gids(gid_list))
 
+    def get_image_properties(ibs, gid_list, key):
+        pass
+
     #---------------------
     # --- Chip Getters ---
     #---------------------
 
     def get_chips(ibs, cid_list):
-        # return raw pixels of chips
+        'returns a list cropped images (numpy arrays)'
         pass
 
-    def get_chip_paths(ibs, gid_list):
-        # return chip paths
+    def get_chip_paths(ibs, cid_list):
+        'returns a list strings (cropped images paths)'
         pass
 
     def get_chip_gids(ibs, cid_list):
+        'returns a list of image ids'
         pass
 
     def get_chip_rois(ibs, cid_list):
+        'returns a list of (x, y, w, h) tuples'
         pass
 
     def get_chip_thetas(ibs, cid_list):
         pass
 
     def get_chip_names(ibs, cid_list):
+        'returns a list of strings ["fred", "sue", ...]'
         pass
 
     def get_chip_masks(ibs, cid_list):
+        # Should this function exist?
         pass
 
-    def get_chip_property(ibs, cid, key):
+    def get_chip_properties(ibs, cid_list, key):
         pass
 
     #---------------------
@@ -115,25 +133,24 @@ class IBEISController(object):
     #---------------------
 
     def get_chips_in_name(ibs, name_list):
+        'returns a list of list of cids in each name'
         # for each name return chips in that name
         pass
 
     def get_num_cids_in_name(ibs, name_list):
         return map(len, ibs.get_chips_in_name(name_list))
 
-
     #--------------------------
     # --- Encounter Getters ---
     #--------------------------
 
-    def get_encounters(ibs):
-        #return eid_list
-        pass
-
-    def get_cids_in_eid(ibs, eid_list):
+    def get_cids_in_eids(ibs, eid_list):
+        'returns a list of list of cids in each encounter'
+        #return cid_list
         pass
 
     def get_gids_in_eids(ibs, eid_list):
+        'returns a list of list of gids in each encounter'
         pass
 
     #-----------------
@@ -146,24 +163,24 @@ class IBEISController(object):
     def delete_images(ibs, gid_list):
         pass
 
-    def delete_names(ibs, name_list):
-        pass
-
     #----------------
     # --- Loaders ---
     #----------------
 
-    def load_from_sql(ibs, self):
+    def load_from_sql(ibs):
+        'Loads chips, images, name, and encounters'
         pass
 
     #----------------
     # --- Writers ---
     #----------------
 
-    def export_to_wildbook(ibs, self):
+    def save_to_sql(ibs, cid_list):
+        'Saves chips, images, name, and encounters'
         pass
 
-    def save_to_sql(ibs, self):
+    def export_to_wildbook(ibs, cid_list):
+        'Exports identified chips to wildbook'
         pass
 
     #--------------
@@ -171,30 +188,50 @@ class IBEISController(object):
     #--------------
 
     def cluster_encounters(ibs, gid_list):
-        pass
+        'Finds encounters'
+        from ibeis.model import encounter_cluster
+        eid_list = encounter_cluster.cluster(ibs, gid_list)
+        #ibs.set_image_eids(gid_list, eid_list)
+        return eid_list
 
-    def query_chips(ibs, qcid_list, dcid_list):
+    def detect_existence(ibs, gid_list, **kwargs):
+        'Detects the probability of animal existence in each image'
+        from ibeis.model import jason_detector
+        probexist_list = jason_detector.detect_existence(ibs, gid_list, **kwargs)
+        # Return for user inspection
+        return probexist_list
+
+    def detect_rois_and_masks(ibs, gid_list, **kwargs):
+        'Runs animal detection in each image'
+        from ibeis.model import jason_detector
+        detections = jason_detector.detect_rois(ibs, gid_list, **kwargs)
+        # detections should be a list of [(gid, roi, theta, mask), ...] tuples
+        # Return for user inspection
+        return detections
+
+    def get_recognition_database_chips(ibs):
+        'returns chips which are part of the persitent recognition database'
+        dcid_list = None
+        return dcid_list
+
+    def query_intra_encounter(ibs, qcid_list, **kwargs):
+        # wrapper
+        dcid_list = qcid_list
+        qres_list = ibs._query_chips(ibs, qcid_list, dcid_list, **kwargs)
+        return qres_list
+
+    def query_database(ibs, qcid_list, **kwargs):
+        # wrapper
+        dcid_list = ibs.get_recognition_database_chips()
+        qres_list = ibs._query_chips(ibs, qcid_list, dcid_list, **kwargs)
+        return qres_list
+
+    def _query_chips(ibs, qcid_list, dcid_list, **kwargs):
         '''
         qcid_list - query chip ids
         dcid_list - database chip ids
         '''
-        #import jon_identifier
-        #jon_identifier.query(ibs, qcid_list, dcid_list)
-        pass
-
-    def detect_existence(ibs, gid_list):
-        #import jason_detector
-        #jason_detector.detect
-        #jason_detector.detect(ibs, gid_list)
-        pass
-
-    def detect_rois_and_masks(ibs, gid_list):
-        #import jason_detector
-        #jason_detector.detect
-        #jason_detector.detect(ibs, gid_list)
-        pass
-
-    def get_recognition_database_chips(ibs):
-        # ...
-        dcid_list = None
-        return dcid_list
+        from ibeis.model import jon_identifier
+        qres_list = jon_identifier.query(ibs, qcid_list, dcid_list, **kwargs)
+        # Return for user inspection
+        return qres_list
