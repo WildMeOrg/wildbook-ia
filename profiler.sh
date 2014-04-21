3c8308f8fb77108e68d2fa4b84d2c05f2fe04a77
Jon Crall <crallj@rpi.edu>
2014-04-21 16:19:38 -0400

    added runsnakerun support to profiler

--- profiler.sh
+++ profiler.sh
@@ -4,13 +4,6 @@ export TIMESTAMP=$(date -d "today" +"%Y-%m-%d_%H-%M-%S")
 export RAW_SUFFIX=raw.prof
 export CLEAN_SUFFIX=clean.prof
 
-# Input
-export pyscript=$(echo $1 | sed -e 's/.*\///g')
-# Output Stage 1
-export line_profile_output=$pyscript.lprof
-# Output Stage 2
-export raw_profile=raw_profile.$pyscript.$TIMESTAMP.$RAW_SUFFIX
-export clean_profile=clean_profile.$pyscript.$TIMESTAMP.$CLEAN_SUFFIX
 
 remove_profiles()
 {
@@ -22,48 +15,70 @@ remove_profiles()
     echo "Finshed removing profiles"
 }
 
-echo "Input: $@"
-
+# Input
+export pyscript=$(echo $1 | sed -e 's/.*\///g')
 
 if [ "$pyscript" = "clean" ]; then
     remove_profiles
     exit
 fi 
 
-echo "pyscript: $pyscript"
-echo "line_profile_output: $line_profile_output"
-echo "Profiling $pyscript"
-
 #set -e
 #/bin/command-that-fails
 
 export SYSNAME="$(expr substr $(uname -s) 1 10)"
-# Choose one
-export PROFILE_TYPE="kernprof"  # plop or kernprof
-#export PROFILE_TYPE="plop"  # plop or kernprof
 
+# Choose one # plop or lineprof, or runsnake
+#export PROFILE_TYPE="runsnake"  
+export PROFILE_TYPE="lineprof"
 
+# Platform independent kernprof
+if [ "$SYSNAME" = "MINGW32_NT" ]; then
+    export MINGW_PYEXE=$(python -c "import sys; print(sys.executable)")
+    export MINGW_PYDIR=$(python -c "import sys, os; print(os.path.dirname(sys.executable))")
+    export KERNPROF_PY="$MINGW_PYEXE $MINGW_PYDIR/Scripts/kernprof.py"
+else
+    export KERNPROF_PY="kernprof.py"
+    export RUNSNAKE_PY="runsnake"
+fi
+
+echo "pyscript: $pyscript"
+
+#
+#
+# PLOP PROFILER
 if [ $PROFILE_TYPE = "plop" ]; then
     python -m plop.collector $@
     echo "http://localhost:8888"
     python -m plop.viewer --datadir=profiles
-elif [ $PROFILE_TYPE = "kernprof" ]; then
+#
+#
+# LINE PROFILER
+elif [ $PROFILE_TYPE = "lineprof" ]; then
+    # Output Stage 1
+    echo "line_profile_output: $line_profile_output"
+    export line_profile_output=$pyscript.lprof
+    # Output Stage 2
+    export raw_profile=raw_profile.$pyscript.$TIMESTAMP.$RAW_SUFFIX
+    export clean_profile=clean_profile.$pyscript.$TIMESTAMP.$CLEAN_SUFFIX
     # Line profile the python code w/ command line args
-    if [ "$SYSNAME" = "MINGW32_NT" ]; then
-        export MINGW_PYEXE=$(python -c "import sys; print(sys.executable)")
-        export MINGW_PYDIR=$(python -c "import sys, os; print(os.path.dirname(sys.executable))")
-        $MINGW_PYEXE $MINGW_PYDIR/Scripts/kernprof.py -l $@
-    else
-        kernprof.py -l $@
-    fi
+    $KERNPROF_PY -l $@
     # Dump the line profile output to a text file
     python -m line_profiler $line_profile_output >> $raw_profile
     # Clean the line profile output
     python _scripts/profiler_cleaner.py $raw_profile $clean_profile
     # Print the cleaned output
     cat $clean_profile
+#
+#
+# PLOP PROFILER
+elif [ $PROFILE_TYPE = "runsnake" ]; then
+    # Line profile the python code w/ command line args
+    export runsnake_profile=RunSnakeContext.$pyscript.$TIMESTAMP.$RAW_SUFFIX
+    python -m cProfile -o $runsnake_profile $@
+    # Dump the line profile output to a text file
+    $RUNSNAKE_PY $runsnake_profile
 fi
-
 
 # In windows I had to set the default .sh extension to run with
 # C:\MinGW\msys\1.0\bin\sh.exe
