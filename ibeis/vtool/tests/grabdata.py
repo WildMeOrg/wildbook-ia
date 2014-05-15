00acbcd165c6fddc513ea6b0e8009aad5b8f101b
Jon Crall <crallj@rpi.edu>
2014-05-15 14:28:17 -0400

    wip

--- ibeis/vtool/tests/grabdata.py
+++ ibeis/vtool/tests/grabdata.py
@@ -13,6 +13,24 @@ def get_testdata_dir(ensure=True):
     return testdata_dir
 
 
+def get_test_gpaths(ndata=None, lena=True, zebra=False, jeff=False):
+    ndata_arg = utool.get_arg('--ndata', type_=int, default=None, help_='use --ndata to specify bigger data')
+    if ndata_arg is not None:
+        ndata = ndata_arg
+    if ndata is None:
+        ndata = 1
+    imgdir = get_testdata_dir()
+    # Build gpath_list
+    gname_list = utool.flatten([
+        ['lena.jpg']  * utool.get_flag('--lena',   lena, help_='add lena to test images'),
+        ['zebra.jpg'] * utool.get_flag('--zebra', zebra, help_='add zebra to test images'),
+        ['jeff.png']  * utool.get_flag('--jeff',   jeff, help_='add jeff to test images'),
+    ])
+    gname_list = utool.util_list.flatten([gname_list] * ndata)
+    gpath_list = utool.fnames_to_fpaths(gname_list, imgdir)
+    return gpath_list
+
+
 def get_testimg_path(gname):
     """
     Returns path to image in testdata
