40dcd2fc3519bcfead03c03cc7b8c410bde20352
Hendrik Weideman <weideh@rpi.edu>
2014-05-19 11:46:58 -0400

    create wildbook occurrences for the ibeis encounters

--- ibeis/export/export_wb.py
+++ ibeis/export/export_wb.py
@@ -13,11 +13,13 @@ print, print_, printDBG, rrr, profile = utool.inject(__name__, '[export_wb]')
 
 
 def export_ibeis_to_wildbook(ibs, eid_list):
+    target_url_occurrence = ibs.wbaddr + '/rest/org.ecocean.Occurrence'
     target_url_encounter = ibs.wbaddr + '/rest/org.ecocean.Encounter'
-    target_url_image = ibs.wbaddr + '/EncounterAddImage'
+    target_url_encounter_add_image = ibs.wbaddr + '/EncounterAddImage'
+    target_url_occurrence_add_encounter = ibs.wbaddr + '/OccurrenceAddEncounter'
+    
     # list of tuples, each has the names associated with the encounter
     nids_list = ibs.get_encounter_nids(eid_list)
-    #images_list = ibs.get_encounter_gids(eid_list)
 
     assert len(nids_list) == len(eid_list)
     for eid, nids in izip(eid_list, nids_list):
@@ -26,38 +28,64 @@ def export_ibeis_to_wildbook(ibs, eid_list):
         # list of list of images that correspond to each name id
         gids_lists = ibs.get_name_gids(nids)
 
+        # each name id must have exactly one text name associated with it, as well as one list of image ids
         assert len(nids) == len(names_text) == len(gids_lists)
+
+        # the actual text encounter name corresponding to the encounter id
+        enctext = ibs.get_encounter_enctext(eid)
+
+        # ibeis encounter is wildbook occurrence!
+        occurrence_id = str(enctext)
+        payload_create_occurrence = {'groupBehavior': '',
+                                     'class': 'org.ecocean.Occurrence',
+                                     'occurrenceID': occurrence_id}
+
+        # create a Wildbook Occurrence to represent this IBEIS Encounter
+        response = requests.post(target_url_occurrence, data=json.dumps(payload_create_occurrence))
+        print ('POSTed IBEIS Encounter / Wildbook Occurrence %s to %s with status %d' % (enctext, target_url_occurrence, response.status_code))
+
         # creates wildbook encounters for each name in an ibeis encounter
         for nid, name_text, gids in izip(nids, names_text, gids_lists):
-            # the actual text encounter name corresponding to the encounter id
-            enctext = ibs.get_encounter_enctext(eid)
+            # create a unique name for this inidividual by concatenating the encounter and name
             wbenc_name = enctext + '_' + str(nid)
-            payload_encounter = {'dwcImageURL': ibs.wbaddr + '/encounters/encounter.jsp?number=' + wbenc_name,
-                                 'state': 'unapproved',
-                                 'dateInMilliseconds': 100,
-                                 'approved': True,
-                                 'catalogNumber': wbenc_name,
-                                 'recordedBy': 'hotspotter',
-                                 'individualID': 'Unassigned',
-                                 'class': 'org.ecocean.Encounter',
-                                 'guid': 'MY_CATALOG:MY_SPECIES:' + wbenc_name,
-                                 'decimalLatitude': '-1.0',
-                                 'decimalLongitude': '-1.0'}
+            payload_create_encounter = {'dwcImageURL': ibs.wbaddr + '/encounters/encounter.jsp?number=' + wbenc_name,
+                                        'occurrenceID': occurrence_id,
+                                        'state': 'unapproved',
+                                        'dateInMilliseconds': 100,
+                                        'approved': True,
+                                        'catalogNumber': wbenc_name,
+                                        'recordedBy': 'hotspotter',
+                                        'individualID': 'Unassigned',
+                                        'class': 'org.ecocean.Encounter',
+                                        'guid': 'MY_CATALOG:MY_SPECIES:' + wbenc_name,
+                                        'decimalLatitude': '-1.0',
+                                        'decimalLongitude': '-1.0'}
 
-            response = requests.post(target_url_encounter, data=json.dumps(payload_encounter))
-            print ('POSTed %s to %s with status %d' % (wbenc_name, target_url_encounter, response.status_code))
+            # create this wildbook encounter
+            response = requests.post(target_url_encounter, data=json.dumps(payload_create_encounter))
+            print ('POSTed Wildbook Encounter %s to %s with status %d' % (wbenc_name, target_url_encounter, response.status_code))
+            
+            payload_occurrence_add_encounter = {'number': wbenc_name,
+                                                'occurrenceID': occurrence_id}
+
+            # link the wildbook encounter to the wildbook occurrence / ibeis encounter
+            response = requests.post(target_url_occurrence_add_encounter, data=payload_occurrence_add_encounter)
+            print ('Linked Wildbook Encounter %s to Wildbook Occurrence %s with status %s' % (wbenc_name, occurrence_id, response.status_code))
+            
             # get the paths to all the images in which this name id appears
             paths = ibs.get_image_paths(gids)
-            payload_image = {'number': wbenc_name}
+            payload_encounter_add_image = {'number': wbenc_name}
+
             # upload the images one by one
             for path in paths:
                 try:
-                    with open(path, 'rb') as my_file:
-                        response = requests.post(target_url_image, data=payload_image, files={'file': my_file})
-                        print ('POSTed %s to %s with status %d' % (path, target_url_image, response.status_code))
+                    with open(path, 'rb') as img_file:
+                        response = requests.post(target_url_encounter_add_image, data=payload_encounter_add_image, files={'file': img_file})
+                        print ('POSTed %s to %s with status %d' % (path, target_url_encounter_add_image, response.status_code))
                 except IOError:
                     print('Could not open file at %s' % (path))
                     continue
+
             # update the wildbook encounter thumbnail image
             response = requests.post(ibs.wbaddr + '/resetThumbnail.jsp?number=' + wbenc_name + '&imageNum=1')
 
