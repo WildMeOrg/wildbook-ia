dc50a5f65889492050cd8e9abeef54c0b39a8b56
joncrall <crallj@rpi.edu>
2014-06-01 20:21:53 -0400

    working on preimage filters

--- ibeis/model/preproc/preproc_image.py
+++ ibeis/model/preproc/preproc_image.py
@@ -42,11 +42,12 @@ def get_image_uuid(img_bytes_):
 
 
 def preprocess_image(tup):
-    gpath, kwargs = tup
-    cache_dir       = kwargs.get('cache_dir', None)
-    max_width       = kwargs.get('max_image_width', None)
-    max_height      = kwargs.get('max_image_height', None)
-    localize_images = kwargs.get('localize_images', False)
+    gpath = tup
+    #gpath, kwargs = tup
+    #cache_dir       = kwargs.get('cache_dir', None)
+    #max_width       = kwargs.get('max_image_width', None)
+    #max_height      = kwargs.get('max_image_height', None)
+    #localize_images = kwargs.get('localize_images', False)
     """ Called in parallel. gpath must be in UNIX-PATH format! """
     try:
         pil_img = Image.open(gpath, 'r')      # Open PIL Image
@@ -58,8 +59,8 @@ def preprocess_image(tup):
         else:
             raise
     width, height  = pil_img.size         # Read width, height
-    if width > max_width or height > max_height:
-        pass
+    #if width > max_width or height > max_height:
+    #    pass
     time, lat, lon = get_exif(pil_img)    # Read exif tags
     img_bytes_ = np.asarray(pil_img).ravel()[::64].tostring()
     image_uuid = get_image_uuid(img_bytes_)  # Read pixels ]-hash-> guid = gid
@@ -70,9 +71,9 @@ def preprocess_image(tup):
     if ext == '.jpeg':
         ext = '.jpg'
     # Move images to the cache dir
-    if localize_images:
-        gname = image_uuid + ext
-        gpath = '/'.join((cache_dir, gname))
+    #if localize_images:
+    #    gname = image_uuid + ext
+    #    gpath = '/'.join((cache_dir, gname))
     param_tup  = (image_uuid, gpath, orig_gname, ext, width, height, time, lat, lon, notes)
     return param_tup
 
@@ -80,5 +81,5 @@ def preprocess_image(tup):
 @profile
 def add_images_params_gen(gpath_list, **kwargs):
     """ generates values for add_images sqlcommands asychronously """
-    preproc_args = [(gpath, kwargs) for gpath in gpath_list]
-    return utool.util_parallel.generate(preprocess_image, preproc_args)
+    #preproc_args = [(gpath, kwargs) for gpath in gpath_list]
+    return utool.util_parallel.generate(preprocess_image, gpath_list)
