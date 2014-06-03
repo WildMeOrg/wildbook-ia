9ec68a77857cd11d61cdd7f347b95e56b74a2df0
joncrall <crallj@rpi.edu>
2014-06-03 08:53:01 -0400

    cleaned test

--- ibeis/plottool/tests/test_viz_image2.py
+++ ibeis/plottool/tests/test_viz_image2.py
@@ -13,25 +13,34 @@ def test_viz_image(img_fpath):
     img = cv2.imread(img_fpath)
     tau = np.pi * 2  # tauday.com
     # Create figure
-    fig = df2.figure(fnum=1, pnum=(1, 1, 1))
+    fig = df2.figure(fnum=42, pnum=(1, 1, 1))
     # Clear figure
     fig.clf()
     # Build parameters
+    bbox_list = [dummy_bbox(img), dummy_bbox(img, (-.25, -.25), .1)]
     showkw = {
-        'title'      : 'test title',
-        'bbox_list'  : [dummy_bbox(img),
-                        dummy_bbox(img, (-.25, -.25), .1)],
+        'title'      : 'test axis title',
+        # The list of bounding boxes to be drawn on the image
+        'bbox_list'  : bbox_list,
         'theta_list' : [tau * .7, tau * .9],
         'sel_list'   : [True, False],
         'label_list' : ['test label', 'lbl2'],
     }
-    viz_image2.show_image(img, fnum=1, pnum=(1, 1, 1), **showkw)
+    # Print the keyword arguments to illustrate their format
+    print('showkw = ' + utool.dict_str(showkw))
+    # Display the image in figure-num 42, using a 1x1 axis grid in the first
+    # axis. Pass showkw as keyword arguments.
+    viz_image2.show_image(img, fnum=42, pnum=(1, 1, 1), **showkw)
+    df2.set_figtitle('Test figure title')
 
 
 if __name__ == '__main__':
     TEST_IMAGES_URL = 'https://dl.dropboxusercontent.com/s/of2s82ed4xf86m6/testdata.zip'
     test_image_dir = utool.grab_zipped_url(TEST_IMAGES_URL, appname='utool')
     imgpaths       = utool.list_images(test_image_dir, fullpath=True)   # test image paths
+    # Get one image filepath to load and display
     img_fpath = imgpaths[0]
+    # Run Test
     test_viz_image(img_fpath)
+    # Magic exec which displays or puts you into IPython with --cmd flag
     exec(df2.present())
