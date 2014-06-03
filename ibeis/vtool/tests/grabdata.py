100e2049adb3bf2251bbf3f5933f3e2c15d666a6
Jon Crall <crallj@rpi.edu>
2014-06-03 14:38:53 -0400

    Enhanced grabdata

--- ibeis/vtool/tests/grabdata.py
+++ ibeis/vtool/tests/grabdata.py
@@ -1,33 +1,37 @@
 from __future__ import absolute_import, division, print_function
 import utool
 from os.path import join, realpath
+from itertools import cycle
 print, print_, printDBG, rrr, profile = utool.inject(__name__, '[testdata]')
 
 
-def get_testdata_dir(ensure=True):
+def get_testdata_dir(ensure=True, key='testdb1'):
     """
     Gets test img directory and downloads it if it doesn't exist
     """
-    zipped_testdata_url = 'https://dl.dropboxusercontent.com/s/of2s82ed4xf86m6/testdata.zip'
+    testdata_map = {
+        'testdb1': 'https://dl.dropboxusercontent.com/s/of2s82ed4xf86m6/testdata.zip'
+    }
+    zipped_testdata_url = testdata_map[key]
     testdata_dir = utool.grab_zipped_url(zipped_testdata_url, ensure=ensure)
     return testdata_dir
 
 
-def get_test_gpaths(ndata=None, lena=True, zebra=False, jeff=False):
+def get_test_gpaths(ndata=None, names=None, **kwargs):
+    # Read ndata from args or command line
     ndata_arg = utool.get_arg('--ndata', type_=int, default=None, help_='use --ndata to specify bigger data')
     if ndata_arg is not None:
         ndata = ndata_arg
-    if ndata is None:
-        ndata = 1
-    imgdir = get_testdata_dir()
-    # Build gpath_list
-    gname_list = utool.flatten([
-        ['lena.jpg']  * utool.get_flag('--lena',   lena, help_='add lena to test images'),
-        ['zebra.jpg'] * utool.get_flag('--zebra', zebra, help_='add zebra to test images'),
-        ['jeff.png']  * utool.get_flag('--jeff',   jeff, help_='add jeff to test images'),
-    ])
-    gname_list = utool.util_list.flatten([gname_list] * ndata)
-    gpath_list = utool.fnames_to_fpaths(gname_list, imgdir)
+    imgdir = get_testdata_dir(**kwargs)
+    gpath_list = sorted(list(utool.list_images(imgdir, full=True)))
+    # Get only the gpaths of certain names
+    if names is not None:
+        gpath_list = [gpath for gpath in gpath_list if
+                      utool.basename_noext(gpath) in names]
+    # Get a some number of test images
+    if ndata is not None:
+        gpath_cycle = cycle(gpath_list)
+        gpath_list  = [gpath_cycle.next() for _ in xrange(ndata)]
     return gpath_list
 
 
