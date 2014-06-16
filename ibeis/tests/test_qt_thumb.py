4ef7b9ab38ef853df03ad6d8ea1432f2b49f9931
--replace-all <rutfiz@rpi.edu>
2014-06-16 16:36:24 -0400

    Working to modify guiheaders to work with trees

--- ibeis/tests/test_qt_thumb.py
+++ ibeis/tests/test_qt_thumb.py
@@ -123,6 +123,8 @@ class IBEISGuiWidget(IBEIS_WIDGET_BASE):
         if model.name == THUMB_TABLE:
             # eid = model.eid
             gid = id_
+            if gid is None:
+                return
             ibswgt.roi_interact = interact_rois2.ROI_Interaction2(ibswgt.ibs, gid)
             print("DOUBLECLICKED ID: %r" % gid)
 
