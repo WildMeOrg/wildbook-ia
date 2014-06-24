8faeadfe4602283a0adc9cb41b45fc4852d1d7fd
Hendrik Weideman <weideh@rpi.edu>
2014-06-24 17:14:30 -0400

    added general image input

--- ibeis/tests/test_ibs_uuid.py
+++ ibeis/tests/test_ibs_uuid.py
@@ -16,71 +16,92 @@ MACHINE_NAME = utool.get_computer_name()
 
 MACHINE_VALS = {}
 MACHINE_VALS['lena'] = {
-    'MACHINE_GPATH' : {
-        'hyrule': '/home/joncrall/.config/utool/testdata/lena.jpg',
+    'GPATH' : {
+        'hyrule':         '/home/joncrall/.config/utool/testdata/lena.jpg',
+        'hendrik-vostro': '/home/hendrik/.config/utool/testdata/lena.jpg',
     },
 
-    'MACHINE_UUIDS': {
-        'hyrule': UUID('d3daf98d-3035-65b7-2ff4-ca4076ab0cf1'),
-        'hendrick': UUID('f2c4658f-a722-793b-8578-c392fd550888'),
+    'UUID': {
+        'hyrule':         UUID('d3daf98d-3035-65b7-2ff4-ca4076ab0cf1'),
+        'hendrik-vostro': UUID('f2c4658f-a722-793b-8578-c392fd550888'),
     },
 
-    'MACHINE_SUMS' : {
-        'hyrule': 18615625,
+    'SUM' : {
+        'hyrule':         18615625,
+        'hendrik-vostro': 18615820,
     },
 
-    'MACHINE_BYTES': {
-        'hyrule': r'\xe2\x98U\xccib\xc8\x7fm\xbfXp\xb1cb\xccbl\xdfTb\xe1;[\xd1ba\xae\x81\xa3\xc1\x84n\xb5df\xcd`k\xd2]m\xe5H^\xd2a^\xd9\x80[\xce\x81Q\xc9e_\xcaNj\xfc]k\xd9N]\xcfb`\xe4}^`\x8f ~~~TRUNCATED~~~ u\x1cJ\xdcq\x86\x8e<<\xde\x1bf\x95^j\xdd\xc7V\xb2',
+    'BYTES': {
+        'hyrule':         r'\xe2\x98U\xccib\xc8\x7fm\xbfXp\xb1cb\xccbl\xdfTb\xe1;[\xd1ba\xae\x81\xa3\xc1\x84n\xb5df\xcd`k\xd2]m\xe5H^\xd2a^\xd9\x80[\xce\x81Q\xc9e_\xcaNj\xfc]k\xd9N]\xcfb`\xe4}^`\x8f ~~~TRUNCATED~~~ u\x1cJ\xdcq\x86\x8e<<\xde\x1bf\x95^j\xdd\xc7V\xb2',
+        'hendrik-vostro': r'\xe2\x99W\xccib\xc8\x7fm\xbfXn\xb1cb\xccbl\xdfTb\xe1;[\xd1aa\xae\x81\xa1\xc1\x84n\xb5df\xcd_m\xd0]m\xe5H^\xd2a^\xd7\x80]\xd1\x81R\xc9e_\xcaNj\xfb]k\xd9N]\xcfb`\xe4}^`\x8f ~~~TRUNCATED~~~ u\x1cJ\xdaq\x86\x8e<<\xde\x1bf\x95^j\xdd\xc8V\xb1',
     },
 
-    'MACHINE_HASH16' : {
-        'hyrule': r'\xd3\xda\xf9\x8d05e\xb7/\xf4\xca@v\xab\x0c\xf1',
+    'HASH16' : {
+        'hyrule':         r'\xd3\xda\xf9\x8d05e\xb7/\xf4\xca@v\xab\x0c\xf1',
+        'hendrik-vostro': r'\xf2\xc4e\x8f\xa7"y;\x85x\xc3\x92\xfdU\x08\x88',
     },
 
-    'MACHINE_HASH20' : {
-        'hyrule': r'\xd3\xda\xf9\x8d05e\xb7/\xf4\xca@v\xab\x0c\xf1\x99\x93\xd5\xcb',
+    'HASH20' : {
+        'hyrule':         r'\xd3\xda\xf9\x8d05e\xb7/\xf4\xca@v\xab\x0c\xf1\x99\x93\xd5\xcb',
+        'hendrik-vostro': r'\xf2\xc4e\x8f\xa7"y;\x85x\xc3\x92\xfdU\x08\x88\xac.sk',
     },
 }
 
-
-def TEST_UUID():
-    print('Machine Name: %r' % MACHINE_NAME)
-
-    print('Image.PILLOW_VERSION: %r' % Image.PILLOW_VERSION)
-    assert Image.PILLOW_VERSION == '1.1.7'
-    print('Image.PILLOW_VERSION: %r' % Image.PILLOW_VERSION)
-    assert Image.PILLOW_VERSION == '1.1.7'
-
+def test_uuid(name):
     print('[TEST_UUID]')
-    gpath = grabdata.get_test_gpaths(names='lena')[0]
-
-    print('gpath = %r' % gpath)
-
+    gpath = grabdata.get_test_gpaths(names=name)[0]
     pil_img = Image.open(gpath)
     uuid1 = preproc_image.get_image_uuid(pil_img)
-    target_uuid = UUID('f2c4658f-a722-793b-8578-c392fd550888')
-    print('uuid1 = %r' % uuid1)
-    print('target_uuid = %r' % target_uuid)
-
-    pil_img = Image.open(gpath)
     npimg = np.asarray(pil_img)
-    print('[ginfo] npimg.sum() = %r' % npimg.sum())
     img_bytes_ = npimg.ravel()[::64].tostring()
-
-    print('img_bytes_ = %r' % (utool.truncate_str(img_bytes_)))
     bytes_sha1 = hashlib.sha1(img_bytes_)
     hashbytes_20 = bytes_sha1.digest()
     hashbytes_16 = hashbytes_20[0:16]
     uuid_ = uuid.UUID(bytes=hashbytes_16)
 
-    print('hashbytes_16 = %r' % (hashbytes_16,))
-    print('hashbytes_20 = %r' % (hashbytes_20,))
-    print('bytes_sha1 = %r' % (bytes_sha1,))
-    print('uuid_ = %r' % (uuid_,))
+    sum_ = npimg.sum()
+    size = pil_img.size
 
-    assert uuid1 == target_uuid, 'uuid and target_uuid do not match'
+    locals_ = locals()
+    def print2(key):
+        val = locals_[key]
+        print('%r: %r' % (MACHINE_NAME, val))
 
-    assert uuid_ == uuid1, 'uuid_ does not match uuid1'
+    print2('gpath')
+    print2('uuid1')
+    print2('size')
+    print2('sum_')
+    print2('img_bytes_')
+    print2('hashbytes_16')
+    print2('hashbytes_20')
+    print2('bytes_sha1')
+    print2('uuid_')
+
+    locals_ = locals()
+    def print2(key):
+        val = locals_[key]
+        print('%r: %r' % (MACHINE_NAME, val))
+    
+    try:
+        target_uuid = MACHINE_VALS[name]['UUID'][MACHINE_NAME]
+        print2('target_uuid')
+        assert uuid1 == target_uuid, 'uuid and target_uuid do not match'
+        assert uuid_ == uuid1, 'uuid_ does not match uuid1'  
+    except KeyError as ex:
+        utool.printex(ex)
+        return False
+
+    return TRUNCATED
+
+def TEST_UUID():
+    print('Machine Name: %r' % MACHINE_NAME)
+
+    print('Image.PILLOW_VERSION: %r' % Image.PILLOW_VERSION)
+    assert Image.PILLOW_VERSION == '2.4.0'
+    print('Image.PILLOW_VERSION: %r' % Image.PILLOW_VERSION)
+    assert Image.PILLOW_VERSION == '2.4.0'
+
+    assert all([test_uuid(name) for name in ['lena', 'jeff']])
 
     return locals()
 
