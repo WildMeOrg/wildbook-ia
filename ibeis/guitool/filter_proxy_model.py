000d820c5e973d8df69c1011edf4c247959a93f3
Avi Weinstock <aweinstock314@gmail.com>
2014-07-10 13:57:16 -0400

    Changes to FilterProxyModel to support queryresult window filtering.

--- ibeis/guitool/filter_proxy_model.py
+++ ibeis/guitool/filter_proxy_model.py
@@ -19,100 +19,32 @@ class FilterProxyModel(BASE_CLASS):
                                              '_change',
                                              '_update',
                                             '_rows_updated',
-                                             'name',],
+                                             'name',
+                                            'get_header_name'],
                                             base_class=BASE_CLASS)
 
-    def __init__(self, parent=None, numduplicates=1):
+    def __init__(self, parent=None):
         BASE_CLASS.__init__(self, parent=parent)
-        self._nd = numduplicates
+        self.filter_dict = {}
 
-    def rowCount(self, parent=QtCore.QModelIndex()):
-        idx = self.mapToSource(parent)
-        source_rows = self.sourceModel().rowCount(parent=idx)
-        rows = math.ceil(source_rows / self._nd)
-        #print('StripeProxyModel.rowCount(): %r %r' % (source_rows, rows))
-        return int(rows)
+    def filterAcceptsRow(self, source_row, source_parent):
+        source = self.sourceModel()
+        row_type = str(source.data(source.index(source_row, 2, parent=source_parent)))
+        print('%r \'%r\'' % (source_row, row_type))
+        print(self.filter_dict)
+        rv = self.filter_dict.get(row_type, True)
+        #print('return value %r' % rv)
+        return rv
 
-    def columnCount(self, parent=QtCore.QModelIndex()):
-        source_cols = self.sourceModel().columnCount(parent=parent)
-        cols = self._nd * source_cols
-        #print('StripeProxyModel.columnCount(): %r %r' % (source_cols, cols))
-        return int(cols)
-
-    def proxy_to_source(self, row, col, parent=QtCore.QModelIndex()):
-        source_model = self.sourceModel()
-        source_cols = source_model.columnCount(parent=parent)
-        r, c, p = row, col, parent
-        r2 = int(math.floor(c / source_cols)) + (r * self._nd)
-        c2 = c % source_cols
-        p2 = p
-        return r2, c2, p2
-
-    def source_to_proxy(self, row, col, parent=QtCore.QModelIndex()):
-        source_model = self.sourceModel()
-        source_cols = source_model.columnCount(parent=parent)
-        r, c, p = row, col, parent
-        r2 = int(math.floor(r / self._nd))
-        c2 = ((r % self._nd) * source_cols) + c
-        p2 = p
-        return r2, c2, p2
-
-    def mapToSource(self, proxyIndex):
-        """
-        returns index into original model
-        """
-        if proxyIndex.isValid():
-            r2, c2, p2 = self.proxy_to_source(proxyIndex.row(), proxyIndex.column())
-            #print('StripeProxyModel.mapToSource(): %r %r %r; %r %r %r' % (r, c, p, r2, c2, p2))
-            idx = self.sourceModel().index(r2, c2, parent=p2)  # self.sourceModel().root_node[r2]
-        else:
-            idx = QtCore.QModelIndex()
-        return idx
-
-    def mapFromSource(self, sourceIndex):
-        """
-        returns index into proxy model
-        """
-        if sourceIndex.isValid():
-            r2, c2, p2 = self.source_to_proxy(sourceIndex.row(), sourceIndex.column(), sourceIndex.parent())
-            #print('StripeProxyModel.mapFromSource(): %r %r %r; %r %r %r' % (r, c, p, r2, c2, p2))
-            idx = self.index(r2, c2, p2)
-        else:
-            idx = QtCore.QModelIndex()
-        return idx
-
-    def filterAcceptsColumn(self, source_column, source_parent):
-        print(source_column, source_parent)
-        return False
-
-    def index(self, row, col, parent=QtCore.QModelIndex()):
-        if (row, col) != (-1, -1):
-            idx = self.createIndex(row, col, parent)
-            #sidx = self.mapToSource(idx)
-            #print('stripeproxymodel.index: (%r, %r) -> (%r, %r)' % (idx.row(), idx.column(), sidx.row(), sidx.column()))
-        else:
-            idx = QtCore.QModelIndex()
-        return idx
-
-    def data(self, proxyIndex, role=Qt.DisplayRole):
-        idx = self.mapToSource(proxyIndex)
-        return self.sourceModel().data(idx, role)
-
-    def setData(self, proxyIndex, value, role=Qt.EditRole):
-        idx = self.mapToSource(proxyIndex)
-        return self.sourceModel().setData(idx, value, role)
-
-    def sort(self, column, order):
-        source_model = self.sourceModel()
-        source_cols = source_model.columnCount()
-        if source_cols > 0:
-            source_model.sort(column % source_cols, order)
-
-    def parent(self, index):
-        return self.sourceModel().parent(self.mapToSource(index))
+    def update_filterdict(self, new_dict):
+        self.filter_dict = new_dict
 
     def _update_rows(self):
         return self.sourceModel()._update_rows()
 
     def _get_row_id(self, proxyIndex):
         return self.sourceModel()._get_row_id(self.mapToSource(proxyIndex))
+
+    def get_header_data(self, colname, qtidx):
+        sidx = self.mapToSource(qtidx)
+        return self.sourceModel().get_header_data(colname, sidx)
