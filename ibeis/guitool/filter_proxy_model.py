cc4e934ee24d93a6b56a7c014550d3ad56065394
Zackary Rutfield <rutfiz@rpi.edu>
2014-07-10 15:25:47 -0400

    Fixed sort in FilterProxyModel

--- ibeis/guitool/filter_proxy_model.py
+++ ibeis/guitool/filter_proxy_model.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, division, print_function
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import Qt
-import math
+from PyQt4 import QtGui
 import utool
 
 #BASE_CLASS = QtGui.QAbstractProxyModel
@@ -11,17 +9,17 @@ BASE_CLASS = QtGui.QSortFilterProxyModel
 
 class FilterProxyModel(BASE_CLASS):
     __metaclass__ = utool.makeForwardingMetaclass(lambda self: self.sourceModel(),
-                                            ['_set_context_id',
-                                             '_get_context_id',
-                                             '_set_changeblocked',
-                                            '_get_changeblocked',
-                                             '_about_to_change',
-                                             '_change',
-                                             '_update',
-                                            '_rows_updated',
-                                             'name',
-                                            'get_header_name'],
-                                            base_class=BASE_CLASS)
+                                                    ['_set_context_id',
+                                                        '_get_context_id',
+                                                        '_set_changeblocked',
+                                                        '_get_changeblocked',
+                                                        '_about_to_change',
+                                                        '_change',
+                                                        '_update',
+                                                        '_rows_updated',
+                                                        'name',
+                                                        'get_header_name'],
+                                                    base_class=BASE_CLASS)
 
     def __init__(self, parent=None):
         BASE_CLASS.__init__(self, parent=parent)
@@ -29,9 +27,9 @@ class FilterProxyModel(BASE_CLASS):
 
     def filterAcceptsRow(self, source_row, source_parent):
         source = self.sourceModel()
-        row_type = str(source.data(source.index(source_row, 2, parent=source_parent)))
-        print('%r \'%r\'' % (source_row, row_type))
-        print(self.filter_dict)
+        row_type = str(source.data(source.index(source_row, 7, parent=source_parent)).typeName())
+        #print('%r \'%r\'' % (source_row, row_type))
+        #print(self.filter_dict)
         rv = self.filter_dict.get(row_type, True)
         #print('return value %r' % rv)
         return rv
@@ -48,3 +46,6 @@ class FilterProxyModel(BASE_CLASS):
     def get_header_data(self, colname, qtidx):
         sidx = self.mapToSource(qtidx)
         return self.sourceModel().get_header_data(colname, sidx)
+
+    def sort(self, column, order):
+        self.sourceModel().sort(column, order)
