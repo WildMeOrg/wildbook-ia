dd5afd43ef596aea3c33131c0ed21cd06a8c4f77
joncrall <crallj@rpi.edu>
2014-08-01 19:57:12 -0400

    wip

--- dev.py
+++ dev.py
@@ -468,7 +468,7 @@ if __name__ == '__main__':
     #
     #
     # Main Loop (IPython interaction, or some exec loop)
-    if '--nopresent' not in sys.argv:
+    if '--nopresent' not in sys.argv or '--noshow' in sys.argv:
         df2.present()
     ipy = ('--gui' not in sys.argv) or ('--cmd' in sys.argv)
     main_execstr = ibeis.main_loop(main_locals, ipy=ipy)
