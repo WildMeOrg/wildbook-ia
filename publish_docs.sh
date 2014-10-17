fb8a223d186ff99f4ebe8d8cf31e5f2b2cb0771f
joncrall <crallj@rpi.edu>
2014-10-17 18:07:15 -0400

    wip

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 autogen_sphinx_docs.py
 cp -r _doc/_build/html/* _page
 #git add _page/.nojekyll
-#git add _page/*
+git add _page/*
 #git add _page
 git commit -m "updated docs"
 git subtree push --prefix _page origin gh-pages
