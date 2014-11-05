c665568b3018a77aabfeb83c01bc6f04c60a3051
joncrall <erotemic@gmail.com>
2014-11-05 14:46:33 -0500

    Added parent to tree node

--- ibeis/guitool/api_tree_node.py
+++ ibeis/guitool/api_tree_node.py
@@ -21,7 +21,7 @@ class TreeNode(TREE_NODE_BASE):
     """
     #__slots__ = ('id_', 'parent_node', 'child_nodes', 'level',)
     def __init__(self, id_, parent_node, level):
-        TREE_NODE_BASE.__init__(self)
+        TREE_NODE_BASE.__init__(self, parent=parent_node)
         self.id_ = id_
         self.parent_node = parent_node
         self.child_nodes = []
