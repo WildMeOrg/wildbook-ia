219acf1449cd414ef71d571501e269f1a3b0b69f
Zach Jablons <zmjjmz@gmail.com>
2014-12-18 14:18:40 -0500

    Finished doctest for ungrouped_gids

--- ibeis/ibsfuncs.py
+++ ibeis/ibsfuncs.py
@@ -1287,9 +1287,13 @@ def get_ungrouped_gids(ibs):
         >>> print("Removing %r" % images_to_remove)
         >>> ibs.unrelate_images_and_encounters(images_to_remove,[nonspecial_eid] * len(images_to_remove))
         >>> ibs.update_special_encounters()
-        >>> print(ibs.get_valid_eids())
-        >>> print(ibs.get_ungrouped_gids())
-        >>> print(ibs.get_special_eids())
+        >>> ungr_eid = ibs.get_encounter_eids_from_text(const.UNGROUPED_IMAGES_ENCTEXT)
+        >>> print("Ungrouped gids %r" % ibs.get_ungrouped_gids())
+        >>> print("Ungrouped eid %d contains %r" % (ungr_eid, ibs.get_encounter_gids([ungr_eid])))
+        >>> ungr_gids = ibs.get_encounter_gids([ungr_eid])[0]
+        >>> assert(sorted(images_to_remove) == sorted(ungr_gids))
+        >>> #TODO Move this part of the test elsewhere
+        >>> #Now let's make sure that when we recompute encounters, our non-special eid remains the same
     """
     special_eids = set(get_special_eids(ibs))
     gid_list = ibs.get_valid_gids()
