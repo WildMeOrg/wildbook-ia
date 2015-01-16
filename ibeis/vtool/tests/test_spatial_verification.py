3e30966bbb3f75edd95842e242c075ea82d15c8e
joncrall <erotemic@gmail.com>
2015-01-16 16:14:41 -0500

    new test sver with better data

--- ibeis/vtool/tests/test_spatial_verification.py
+++ ibeis/vtool/tests/test_spatial_verification.py
@@ -19,6 +19,48 @@ ori_thresh = ktool.KPTS_DTYPE(np.tau / 4)
 
 
 def test_sver(chip1, chip2, kpts1, kpts2, fm, nShow=6):
+    r"""
+    Args:
+        chip1 (ndarray[uint8_t, ndim=2]):  annotation image data
+        chip2 (ndarray[uint8_t, ndim=2]):  annotation image data
+        kpts1 (ndarray[float32_t, ndim=2]):  keypoints
+        kpts2 (ndarray[float32_t, ndim=2]):  keypoints
+        fm (list):  list of feature matches as tuples (qfx, dfx)
+        nShow (int):
+
+    Returns:
+        ?:
+
+    CommandLine:
+        python -m vtool.tests.test_spatial_verification --test-test_sver:0 --show
+        python -m vtool.tests.test_spatial_verification --test-test_sver:1 --show
+
+    Example0:
+        >>> # DISABLE_DOCTEST
+        >>> import plottool as pt
+        >>> from vtool.tests.test_spatial_verification import *  # NOQA
+        >>> # build test data
+        >>> nShow = utool.get_argval('--nShow', int, 1)
+        >>> chip1, chip2, kpts1, kpts2, fm = get_dummy_test_vars()
+        >>> # execute function
+        >>> result = test_sver(chip1, chip2, kpts1, kpts2, fm, nShow)
+        >>> # verify results
+        >>> print(result)
+        >>> pt.show_if_requested()
+
+    Example1:
+        >>> # DISABLE_DOCTEST
+        >>> import plottool as pt
+        >>> from vtool.tests.test_spatial_verification import *  # NOQA
+        >>> # build test data
+        >>> nShow = utool.get_argval('--nShow', int, 1)
+        >>> chip1, chip2, kpts1, kpts2, fm = get_dummy_test_vars1()
+        >>> # execute function
+        >>> result = test_sver(chip1, chip2, kpts1, kpts2, fm, nShow)
+        >>> # verify results
+        >>> print(result)
+        >>> pt.show_if_requested()
+    """
 
     xy_thresh_sqrd = ktool.get_diag_extent_sqrd(kpts2) * xy_thresh
 
@@ -87,22 +129,78 @@ def get_dummy_test_vars():
     return chip1, chip2, kpts1, kpts2, fm
 
 
+def get_dummy_test_vars1(fname1='easy1.png', fname2='easy2.png'):
+    import utool as ut
+    from vtool import image as gtool
+    from vtool import features as feattool
+    fpath1 = ut.grab_test_imgpath(fname1)
+    fpath2 = ut.grab_test_imgpath(fname2)
+    kpts1, vecs1 = feattool.extract_features(fpath1)
+    kpts2, vecs2 = feattool.extract_features(fpath2)
+    chip1 = gtool.imread(fpath1)
+    chip2 = gtool.imread(fpath2)
+    #chip1_shape = vt.gtool.open_image_size(fpath1)
+    #chip2_shape = gtool.open_image_size(fpath2)
+    #dlen_sqrd2 = chip2_shape[0] ** 2 + chip2_shape[1]
+    #testtup = (rchip1, rchip2, kpts1, vecs1, kpts2, vecs2, dlen_sqrd2)
+    import vtool as vt
+    checks = 800
+    flann_params = {
+        'algorithm': 'kdtree',
+        'trees': 8
+    }
+    #pseudo_max_dist_sqrd = (np.sqrt(2) * 512) ** 2
+    pseudo_max_dist_sqrd = 2 * (512 ** 2)
+    flann = vt.flann_cache(vecs1, flann_params=flann_params)
+    import pyflann
+    try:
+        fx2_to_fx1, _fx2_to_dist = flann.nn_index(vecs2, num_neighbors=2, checks=checks)
+    except pyflann.FLANNException:
+        print('vecs1.shape = %r' % (vecs1.shape,))
+        print('vecs2.shape = %r' % (vecs2.shape,))
+        print('vecs1.dtype = %r' % (vecs1.dtype,))
+        print('vecs2.dtype = %r' % (vecs2.dtype,))
+        raise
+    fx2_to_dist = np.divide(_fx2_to_dist, pseudo_max_dist_sqrd)
+    fx2_to_ratio = np.divide(fx2_to_dist.T[0], fx2_to_dist.T[1])
+    ratio_thresh = .625
+    fx2_to_isvalid = fx2_to_ratio < ratio_thresh
+    fx2_m = np.where(fx2_to_isvalid)[0]
+    fx1_m = fx2_to_fx1.T[0].take(fx2_m)
+    #fs_RAT = np.subtract(1.0, fx2_to_ratio.take(fx2_m))
+    fm_RAT = np.vstack((fx1_m, fx2_m)).T
+    fm = fm_RAT
+    return chip1, chip2, kpts1, kpts2, fm
+
+
 def get_stashed_test_vars():
     chip1, chip2, kpts1, kpts2, fm, homog_tup, aff_tup = utool.load_testdata(
         'chip1', 'chip2', 'kpts1', 'kpts2', 'fm', 'homog_tup', 'aff_tup')
     return chip1, chip2, kpts1, kpts2, fm
 
 
+#if __name__ == '__main__':
+#    """
+#    CommandLine:
+#        python -m vtool.tests.test_spatial_verification
+#    """
+#    np.set_printoptions(precision=4, suppress=True)
+#    utool.util_inject.inject_colored_exceptions()
+#    nShow = utool.get_argval('--nShow', int, 1)
+#    chip1, chip2, kpts1, kpts2, fm = get_dummy_test_vars()
+#    #chip1, chip2, kpts1, kpts2, fm = get_stashed_test_vars()
+#    test_locals = test_sver(chip1, chip2, kpts1, kpts2, fm, nShow=nShow)
+#    exec(utool.execstr_dict(test_locals, 'test_locals'))
+#    exec(df2.present())
+
 if __name__ == '__main__':
     """
     CommandLine:
         python -m vtool.tests.test_spatial_verification
+        python -m vtool.tests.test_spatial_verification --allexamples
+        python -m vtool.tests.test_spatial_verification --allexamples --noface --nosrc
     """
-    np.set_printoptions(precision=4, suppress=True)
-    utool.util_inject.inject_colored_exceptions()
-    nShow = utool.get_argval('--nShow', int, 1)
-    chip1, chip2, kpts1, kpts2, fm = get_dummy_test_vars()
-    #chip1, chip2, kpts1, kpts2, fm = get_stashed_test_vars()
-    test_locals = test_sver(chip1, chip2, kpts1, kpts2, fm, nShow=nShow)
-    exec(utool.execstr_dict(test_locals, 'test_locals'))
-    exec(df2.present())
+    import multiprocessing
+    multiprocessing.freeze_support()  # for win32
+    import utool as ut  # NOQA
+    ut.doctest_funcs()
