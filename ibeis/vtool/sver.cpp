cbfc32cf809f5ddb4c11f36404e44ae264f528ce
Avi Weinstock <aweinstock314@gmail.com>
2015-01-22 17:10:53 -0500

    Progress on sver.cpp, matrix output is now populated.

--- ibeis/vtool/sver.cpp
+++ ibeis/vtool/sver.cpp
@@ -17,14 +17,27 @@ indices into kpts{1,2} indicating a match
 #include <cmath>
 #include <cstdio>
 #include <opencv2/core/core.hpp>
+#include <vector>
 
 using cv::Matx;
+using std::vector;
 
-template<typename _Tp> Matx<_Tp, 3, 3> get_invV_mat(_Tp a, _Tp c, _Tp d, _Tp x, _Tp y, _Tp theta) {
-    _Tp ct = (_Tp)cos(theta), st = (_Tp)sin(theta);
+// adapted from hesaff's helpers.h
+#ifndef M_TAU
+#define M_TAU 6.28318
+#endif
+template <class T> T ensure_0toTau(T x)
+{
+    if(x < 0) { return ensure_0toTau(x+M_TAU); }
+    else if(x >= M_TAU) { return ensure_0toTau(x-M_TAU); }
+    else { return x; }
+}
+
+template<typename T> Matx<T, 3, 3> get_invV_mat(T x, T y, T a, T c, T d, T theta) {
+    T ct = (T)cos(theta), st = (T)sin(theta);
     // https://github.com/aweinstock314/haskell-stuff/blob/master/ExpressionSimplifier.hs
-    return Matx<_Tp, 3, 3>(
-             a*ct,    a*(-st),   x,
+    return Matx<T, 3, 3>(
+             a*ct,      a*(-st),   x,
         c*ct+d*st, c*(-st)+d*ct,   y,
               0.0,        0.0, 1.0);
 }
@@ -44,33 +57,112 @@ void debug_print_mat3x3(const char* name, const Matx<double, 3, 3>& mat) {
         mat(2, 0), mat(2, 1), mat(2, 2));
 }
 
-template<typename _Tp> _Tp xy_distance(Matx<_Tp, 3, 3> kpt1, Matx<_Tp, 3, 3> kpt2) {
-    _Tp x1 = kpt1(0, 2), y1 = kpt1(1, 2);
-    _Tp x2 = kpt2(0, 2), y2 = kpt2(1, 2);
-    _Tp dx = x2-x1, dy = y2-y1;
-    return sqrt(dx*dx + dy*dy);
+template<typename T> T xy_distance(Matx<T, 3, 3> kpt1, Matx<T, 3, 3> kpt2) {
+    // ktool.get_invVR_mats_xys
+    T x1 = kpt1(0, 2), y1 = kpt1(1, 2);
+    T x2 = kpt2(0, 2), y2 = kpt2(1, 2);
+    // ltool.L2_sqrd
+    T dx = x2-x1, dy = y2-y1;
+    return dx*dx + dy*dy;
+}
+
+template<typename T> T det_distance(Matx<T, 3, 3> kpt1, Matx<T, 3, 3> kpt2) {
+    // ktool.get_invVR_mats_sqrd_scale
+    T a1 = kpt1(0, 0), b1 = kpt1(0, 1), c1 = kpt1(1, 0), d1 = kpt1(1, 1);
+    T a2 = kpt2(0, 0), b2 = kpt2(0, 1), c2 = kpt2(1, 0), d2 = kpt2(1, 1);
+    T det1 = a1*d1 - b1*c1, det2 = a2*d2 - b2*c2;
+    // ltool.det_distance
+    T dist = det1/det2;
+    if(dist < 1) { dist = 1/dist; }
+    return dist;
+}
+
+template<typename T> T ori_distance(Matx<T, 3, 3> kpt1, Matx<T, 3, 3> kpt2) {
+    // ktool.get_invVR_mats_oris
+    T a1 = kpt1(0, 0), b1 = kpt1(0, 1);
+    T a2 = kpt2(0, 0), b2 = kpt2(0, 1);
+    T ori1 = ensure_0toTau(-atan2(b1, a1));
+    T ori2 = ensure_0toTau(-atan2(b2, a2));
+    // ltool.ori_distance
+    T delta = fabs(ori1 - ori2);
+    delta = ensure_0toTau(delta);
+    return std::min(delta, M_TAU-delta);
 }
 
 extern "C" {
     void get_affine_inliers(double* kpts1, size_t kpts1_len,
                     double* kpts2, size_t kpts2_len,
                     size_t* fm, size_t fm_len,
-                    double xy_thresh_sqrd, double scale_thresh_sqrd, double ori_thresh)
+                    double xy_thresh_sqrd, double scale_thresh_sqrd, double ori_thresh,
+                    // memory is expected to by allocated by the caller (i.e. via numpy.empty)
+                    size_t* out_inliers_list, double* out_errors_list, double* out_matrices_list)
     {
+// remove some redundancy in a possibly-ugly way
+#define SETUP_RELEVANT_VARIABLES \
+double* kpt1 = &kpts1[6*fm[fm_ind+0]]; \
+double* kpt2 = &kpts2[6*fm[fm_ind+1]]; \
+Matx<double, 3, 3> invVR1_m = get_invV_mat( \
+    kpt1[0], kpt1[1], kpt1[2], \
+    kpt1[3], kpt1[4], kpt1[5]); \
+Matx<double, 3, 3> invVR2_m = get_invV_mat( \
+    kpt2[0], kpt2[1], kpt2[2], \
+    kpt2[3], kpt2[4], kpt2[5]); \
+Matx<double, 3, 3> V1_m = invVR1_m.inv();
+        vector<Matx<double, 3, 3> > Aff_mats;
+        vector<vector<double> > xy_errs, scale_errs, ori_errs;
         for(size_t fm_ind = 0; fm_ind < fm_len; fm_ind += 2) {
-            //printf("fm[[%u, %u]] == [%u, %u]\n", fm_ind, fm_ind+1, fm[fm_ind], fm[fm_ind+1]);
-            double* kpt1 = &kpts1[6*fm[fm_ind+0]];
-            double* kpt2 = &kpts2[6*fm[fm_ind+1]];
-            Matx<double, 3, 3> invVR1_m = get_invV_mat(
-                kpt1[0], kpt1[1], kpt1[2],
-                kpt1[3], kpt1[4], kpt1[5]);
-            Matx<double, 3, 3> invVR2_m = get_invV_mat(
-                kpt2[0], kpt2[1], kpt2[2],
-                kpt2[3], kpt2[4], kpt2[5]);
-            Matx<double, 3, 3> V1_m = invVR1_m.inv();
+            SETUP_RELEVANT_VARIABLES
             Matx<double, 3, 3> Aff_mat = invVR2_m * V1_m;
-            debug_print_mat3x3("Aff_mat", Aff_mat);
+            Aff_mats.push_back(Aff_mat);
         }
+        for(size_t i = 0; i < Aff_mats.size(); i++) {
+            xy_errs.push_back(vector<double>());
+            scale_errs.push_back(vector<double>());
+            ori_errs.push_back(vector<double>());
+            for(size_t fm_ind = 0; fm_ind < fm_len; fm_ind += 2) {
+                SETUP_RELEVANT_VARIABLES
+                Matx<double, 3, 3> Aff_mat = Aff_mats[i];
+                // _test_hypothesis_inliers
+                Matx<double, 3, 3> invVR1_mt = Aff_mat * invVR1_m;
+                double xy_err = xy_distance(invVR1_mt, invVR2_m);
+                double scale_err = det_distance(invVR1_mt, invVR2_m);
+                double ori_err = ori_distance(invVR1_mt, invVR2_m);
+                xy_errs[i].push_back(xy_err);
+                scale_errs[i].push_back(scale_err);
+                ori_errs[i].push_back(ori_err);
+                //printf("errs[%u][%u]: %f, %f, %f\n", fm_ind, i, xy_err, scale_err, ori_err);
+            }
+        }
+/*
+#define SHOW_ERRVEC(vec) \
+for(size_t i = 0; i < vec.size(); i++) { \
+    putchar('['); \
+    for(size_t j = 0; j < vec[i].size(); j++) { \
+        printf("%f, ", vec[i][j]); \
+    } \
+    puts("]"); \
+}
+        SHOW_ERRVEC(xy_errs)
+        SHOW_ERRVEC(scale_errs)
+        SHOW_ERRVEC(ori_errs)
+#undef SHOW_ERRVEC
+*/
+        printf("%d\n", Aff_mats.size());
+        for(size_t i = 0; i < Aff_mats.size(); i++) {
+            const size_t mat_size = 3*3*sizeof(double);
+            //char msg[] = {'M', 'a', 't', 0x30+i%10, 0};
+            //debug_print_mat3x3(msg, Aff_mats[i]);
+            double* dest = (out_matrices_list+(3*3*i));
+            //char* destc = (char*)(out_matrices_list+(3*3*i));
+            //printf("%x\n", dest);
+            //printf("before: "); for(size_t j=0; j < 9; j++) {printf("%f ", *(dest+j)); }
+            //printf("\nbefore: "); for(size_t j=0; j < mat_size; j+=8) {printf("0x%08x ", *(destc+j)); }
+            memcpy(dest, &Aff_mats[i], mat_size);
+            //printf("\nafter: "); for(size_t j=0; j < 9; j++) {printf("%f ", *(dest+j)); }
+            //printf("\nafter: "); for(size_t j=0; j < mat_size; j+=8) {printf("0x%08x ", *(destc+j)); }
+            //puts("\n");
+        }
+#undef SETUP_RELEVANT_VARIABLES
     }
 
     void hello_world() {
--- ibeis/vtool/sver_c_wrapper.py
+++ ibeis/vtool/sver_c_wrapper.py
@@ -3,6 +3,8 @@ import ctypes as C
 import numpy as np
 import vtool.keypoint as ktool
 import vtool.tests.dummy as dummy
+import vtool.spatial_verification as sver
+import utool as ut
 
 TAU = 2 * np.pi  # tauday.org
 c_double_p = C.POINTER(C.c_double)
@@ -10,21 +12,39 @@ c_double_p = C.POINTER(C.c_double)
 # copied/adapted from _pyhesaff.py
 FLAGS_RW = 'aligned, c_contiguous, writeable'
 kpts_t = np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags=FLAGS_RW)
-fms_t = np.ctypeslib.ndpointer(dtype=np.uint, ndim=2, flags=FLAGS_RW)
+fms_t = np.ctypeslib.ndpointer(dtype=np.int64, ndim=2, flags=FLAGS_RW)
+
+inliers_t = np.ctypeslib.ndpointer(dtype=np.int64, ndim=2, flags=FLAGS_RW)
+errs_t = np.ctypeslib.ndpointer(dtype=np.float64, ndim=3, flags=FLAGS_RW)
+mats_t = np.ctypeslib.ndpointer(dtype=np.float64, ndim=3, flags=FLAGS_RW)
 
 def test_calling():
     kpts1, kpts2 = dummy.get_dummy_kpts_pair((100, 100))
-    fm = np.ascontiguousarray(dummy.make_dummy_fm(len(kpts1)).astype(np.uint))
+    #fm = np.ascontiguousarray(dummy.make_dummy_fm(len(kpts1)).astype(np.uint))
+    fm = np.ascontiguousarray(dummy.make_dummy_fm(len(kpts1)).astype(np.int64))
     xy_thresh_sqrd = ktool.KPTS_DTYPE(.009) ** 2
     scale_thresh_sqrd = ktool.KPTS_DTYPE(2)
     ori_thresh = ktool.KPTS_DTYPE(TAU / 4)
-    print(repr([kpts1, kpts2, fm, xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh]))
-    #output = get_affine_inliers(kpts1, kpts2, fm, xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh)
+    #print(repr([kpts1, kpts2, fm, xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh]))
+    output = sver.get_affine_inliers(kpts1, kpts2, fm, xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh)
     c_sver = C.cdll['./sver.so']
     gai = c_sver['get_affine_inliers']
     gai.restype = None
-    gai.argtypes = [kpts_t, C.c_size_t, kpts_t, C.c_size_t, fms_t, C.c_size_t, C.c_double, C.c_double, C.c_double]
-    gai(kpts1, len(kpts1), kpts2, len(kpts2), fm, len(fm), xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh)
+    gai.argtypes = [kpts_t, C.c_size_t,
+                    kpts_t, C.c_size_t,
+                    fms_t, C.c_size_t,
+                    C.c_double, C.c_double, C.c_double,
+                    inliers_t, errs_t, mats_t]
+    out_inliers = np.empty((len(fm), 1), np.int64)
+    out_errors = np.empty((len(fm), 3, len(fm)), np.float64)
+    out_mats = np.empty((len(fm), 3, 3), np.float64)
+    gai(kpts1, 6*len(kpts1),
+        kpts2, 6*len(kpts2),
+        fm, 2*len(fm),
+        xy_thresh_sqrd, scale_thresh_sqrd, ori_thresh,
+        out_inliers, out_errors, out_mats)
+    assert np.allclose(out_mats, output[2])
+    ut.embed()
 
 def call_hello():
     lib = C.cdll['./sver.so']
