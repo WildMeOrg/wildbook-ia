6fb3a7004cf32a9b6fdfb3c032a4029379e2f3bc
joncrall <erotemic@gmail.com>
2015-02-19 16:32:38 -0500

    Added draw verts unit test

--- ibeis/vtool/geometry.py
+++ ibeis/vtool/geometry.py
@@ -61,6 +61,36 @@ def unhomogonize(xyz_arr):
 
 
 def draw_verts(img_in, verts, color=(0, 128, 255), thickness=2):
+    r"""
+    Args:
+        img_in (?):
+        verts (?):
+        color (tuple):
+        thickness (int):
+
+    Returns:
+        ndarray[uint8_t, ndim=2]: img -  image data
+
+    CommandLine:
+        python -m vtool.geometry --test-draw_verts --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from vtool.geometry import *  # NOQA
+        >>> import plottool as pt
+        >>> import vtool as vt
+        >>> # build test data
+        >>> img_in = vt.imread(ut.grab_test_imgpath('carl.jpg'))
+        >>> verts = ((10, 10), (10, 100), (100, 100), (100, 10))
+        >>> color = (0, 128, 255)
+        >>> thickness = 2
+        >>> # execute function
+        >>> img = draw_verts(img_in, verts, color, thickness)
+        >>> # verify results
+        >>> if ut.show_was_requested():
+        >>>     pt.imshow(img)
+        >>>     pt.show_if_requested()
+    """
     img = np.copy(img_in)
     if isinstance(verts, np.ndarray):
         verts = verts.tolist()
@@ -71,3 +101,16 @@ def draw_verts(img_in, verts, color=(0, 128, 255), thickness=2):
         #print('p1, p2: (%r, %r)' % (p1, p2))
         cv2.line(img, tuple(p1), tuple(p2), color, thickness)
     return img
+
+
+if __name__ == '__main__':
+    """
+    CommandLine:
+        python -m vtool.geometry
+        python -m vtool.geometry --allexamples
+        python -m vtool.geometry --allexamples --noface --nosrc
+    """
+    import multiprocessing
+    multiprocessing.freeze_support()  # for win32
+    import utool as ut  # NOQA
+    ut.doctest_funcs()
