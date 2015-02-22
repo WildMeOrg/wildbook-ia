807a12d563fe87448a4de9caaf80bf1a162af676
joncrall <erotemic@gmail.com>
2015-02-22 12:18:14 -0500

    Fixed memory leak in QThumbDelegate

--- ibeis/guitool/api_thumb_delegate.py
+++ ibeis/guitool/api_thumb_delegate.py
@@ -1,4 +1,7 @@
 """
+CommandLine:
+    main.py --db PZ_MUGU_19 --eid 13 --verbthumb
+
 Notes:
     http://stackoverflow.com/questions/8312725/how-to-create-executable-file-for-a-qt-application
     http://stackoverflow.com/questions/6783194/background-thread-with-qthread-in-pyqt
@@ -10,7 +13,7 @@ Notes:
 """
 from __future__ import absolute_import, division, print_function
 from guitool.__PYQT__ import QtGui, QtCore
-import cv2
+import cv2  # NOQA
 import numpy as np
 import utool
 #import time
@@ -25,22 +28,97 @@ from vtool import geometry
 import utool as ut
 ut.noinject(__name__, '[APIThumbDelegate]', DEBUG=False)
 
-VERBOSE = utool.VERBOSE or ut.get_argflag(('--verbose-qt', '--verbqt'))
+
+VERBOSE_QT = ut.get_argflag(('--verbose-qt', '--verbqt'))
+VERBOSE_THUMB = utool.VERBOSE or ut.get_argflag(('--verbose-thumb', '--verbthumb')) or VERBOSE_QT
 
 
 MAX_NUM_THUMB_THREADS = 1
 
 
+def read_thumb_size(thumb_path):
+    if VERBOSE_THUMB:
+        print('[ThumbDelegate] Reading thumb size')
+    npimg = gtool.imread(thumb_path, delete_if_corrupted=True)
+    (height, width) = npimg.shape[0:2]
+    del npimg
+    return width, height
+
+
+def test_show_qimg(qimg):
+    qpixmap = QtGui.QPixmap(qimg)
+    lbl = QtGui.QLabel()
+    lbl.setPixmap(qpixmap)
+    lbl.show()   # show label with qim image
+    return lbl
+
+
+#@ut.memprof
 def read_thumb_as_qimg(thumb_path):
+    r"""
+    Args:
+        thumb_path (?):
+
+    Returns:
+        tuple: (qimg, width, height)
+
+    CommandLine:
+        python -m guitool.api_thumb_delegate --test-read_thumb_as_qimg --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from guitool.api_thumb_delegate import *  # NOQA
+        >>> import guitool
+        >>> # build test data
+        >>> thumb_path = ut.grab_test_imgpath('carl.jpg')
+        >>> # execute function
+        >>> guitool.ensure_qtapp()
+        >>> (qimg) = ut.memprof(read_thumb_as_qimg)(thumb_path)
+        >>> if ut.show_was_requested():
+        >>>    lbl = test_show_qimg(qimg)
+        >>>    guitool.qtapp_loop()
+        >>> # verify results
+        >>> print(qimg)
+
+    Timeit::
+        %timeit np.dstack((npimg, np.full(npimg.shape[0:2], 255, dtype=np.uint8)))
+        %timeit cv2.cvtColor(npimg, cv2.COLOR_BGR2BGRA)
+        npimg1 = np.dstack((npimg, np.full(npimg.shape[0:2], 255, dtype=np.uint8)))
+        # seems to be memory leak in cvtColor?
+        npimg2 = cv2.cvtColor(npimg, cv2.COLOR_BGR2BGRA)
+
+    """
+    if VERBOSE_THUMB:
+        print('[ThumbDelegate] Reading thumb as qimg')
     # Read thumbnail image and convert to 32bit aligned for Qt
-    npimg   = gtool.imread(thumb_path, delete_if_corrupted=True)
-    npimg   = cv2.cvtColor(npimg, cv2.COLOR_BGR2BGRA)
-    data    = npimg.astype(np.uint8)
-    (height, width, nDims) = npimg.shape[0:3]
-    npimg   = np.dstack((npimg[:, :, 3], npimg[:, :, 0:2]))
-    format_ = QtGui.QImage.Format_ARGB32
-    qimg    = QtGui.QImage(data, width, height, format_)
-    return qimg, width, height
+    #if False:
+    #    data  = np.dstack((npimg, np.full(npimg.shape[0:2], 255, dtype=np.uint8)))
+    if False:
+        # Reading the npimage and then handing it off to Qt causes a memory
+        # leak. The numpy array probably is never unallocated because qt doesn't
+        # own it and it never loses its reference count
+        npimg = gtool.imread(thumb_path, delete_if_corrupted=True)
+        print('npimg.dtype = %r, %r' % (npimg.shape, npimg.dtype))
+        npimg   = cv2.cvtColor(npimg, cv2.COLOR_BGR2BGRA)
+        format_ = QtGui.QImage.Format_ARGB32
+        #    #data    = npimg.astype(np.uint8)
+        #    #npimg   = np.dstack((npimg[:, :, 3], npimg[:, :, 0:2]))
+        #    #data    = npimg.astype(np.uint8)
+        #else:
+        # Memory seems to be no freed by the QImage?
+        #data = np.ascontiguousarray(npimg[:, :, ::-1].astype(np.uint8), dtype=np.uint8)
+        #data = np.ascontiguousarray(npimg[:, :, :].astype(np.uint8), dtype=np.uint8)
+        data = npimg
+        #format_ = QtGui.QImage.Format_RGB888
+        (height, width) = data.shape[0:2]
+        qimg    = QtGui.QImage(data, width, height, format_)
+        del npimg
+        del data
+    else:
+        format_ = QtGui.QImage.Format_ARGB32
+        #qimg    = QtGui.QImage(thumb_path, format_)
+        qimg    = QtGui.QImage(thumb_path)
+    return qimg
 
 
 RUNNING_CREATION_THREADS = {}
@@ -69,8 +147,8 @@ class APIThumbDelegate(DELEGATE_BASE):
     requested thumbnail size is
     """
     def __init__(dgt, parent=None, get_thumb_size=None):
-        if VERBOSE:
-            print('[ThumbDelegate] __init__')
+        if VERBOSE_THUMB:
+            print('[ThumbDelegate] __init__ parent=%r, get_thumb_size=%r' % (parent, get_thumb_size))
         DELEGATE_BASE.__init__(dgt, parent)
         dgt.pool = None
         if get_thumb_size is None:
@@ -113,12 +191,13 @@ class APIThumbDelegate(DELEGATE_BASE):
         try:
             data = dgt.get_model_data(qtindex)
             if data is None:
+                print('[thumb_delegate] no data')
                 return
             (thumb_path, img_path, img_size, bbox_list, theta_list) = data
-            valid = (thumb_path is None or img_path is None or bbox_list is None
-                     or img_size is None)
-            if valid:
-                #print('[thumb_delegate] something is wrong')
+            invalid = (thumb_path is None or img_path is None or bbox_list is None
+                       or img_size is None)
+            if invalid:
+                print('[thumb_delegate] something is wrong')
                 return
         except AssertionError as ex:
             utool.printex(ex, 'error getting thumbnail data')
@@ -130,12 +209,14 @@ class APIThumbDelegate(DELEGATE_BASE):
 
         if not exists(thumb_path):
             if not exists(img_path):
-                if VERBOSE:
+                if VERBOSE_THUMB:
                     print('[ThumbDelegate] SOURCE IMAGE NOT COMPUTED: %r' % (img_path,))
                 return None
             # Start computation of thumb if needed
             #qtindex.model()._update()  # should probably be deleted
             # where you are when you request the run
+            if VERBOSE_THUMB:
+                print('[ThumbDelegate] Spawning thumbnail creation thread')
             thumbsize = dgt.get_thumb_size()
             thumb_creation_thread = ThumbnailCreationThread(
                 thumb_path,
@@ -172,7 +253,8 @@ class APIThumbDelegate(DELEGATE_BASE):
                 if view_would_not_be_visible(view, offset):
                     return None
                 # Read the precomputed thumbnail
-                qimg, width, height = read_thumb_as_qimg(thumb_path)
+                qimg = read_thumb_as_qimg(thumb_path)
+                width, height = qimg.width(), qimg.height()
                 view = dgt.parent()
                 if isinstance(view, QtGui.QTreeView):
                     col_width = view.columnWidth(qtindex.column())
@@ -218,8 +300,7 @@ class APIThumbDelegate(DELEGATE_BASE):
             thumb_path = dgt.try_get_thumb_path(view, offset, qtindex)
             if thumb_path is not None:
                 # Read the precomputed thumbnail
-                qimg, width, height = read_thumb_as_qimg(thumb_path)
-                del qimg
+                width, height = read_thumb_size(thumb_path)
                 return QtCore.QSize(width, height)
             else:
                 #print("[APIThumbDelegate] Name not found")
@@ -323,3 +404,16 @@ class ThumbnailCreationThread(RUNNABLE_BASE):
 #print('[APIItemDelegate] Request Thumb: rc=(%d, %d), nBboxes=%r' %
 #      (qtindex.row(), qtindex.column(), len(bbox_list)))
 #print('[APIItemDelegate] bbox_list = %r' % (bbox_list,))
+
+
+if __name__ == '__main__':
+    """
+    CommandLine:
+        python -m guitool.api_thumb_delegate
+        python -m guitool.api_thumb_delegate --allexamples
+        python -m guitool.api_thumb_delegate --allexamples --noface --nosrc
+    """
+    import multiprocessing
+    multiprocessing.freeze_support()  # for win32
+    import utool as ut  # NOQA
+    ut.doctest_funcs()
