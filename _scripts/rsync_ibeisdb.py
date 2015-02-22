7cfe1275f3fa2bca5bc1cb10a4d7e940a0cd45cd
joncrall <erotemic@gmail.com>
2015-02-22 13:31:07 -0500

    Added push and pull option to rsync_ibeis_db

--- _scripts/rsync_ibeisdb.py
+++ _scripts/rsync_ibeisdb.py
@@ -6,79 +6,91 @@ CommandLine:
     python _scripts/rsync_ibeisdb.py --dryrun
 """
 import utool as ut
-import os.path
 
 
 DRY_RUN = ut.get_argflag('--dryrun')
 
 
-def rsync_pull(host, remote_dpath, local_dpath, dname, exclude_dirs=[], dryrun=DRY_RUN):
+def rsync(src_uri, dst_uri, exclude_dirs=[], dryrun=DRY_RUN):
     """
-    General function to pull a directory from a remote server to a local path
+    rsync [OPTION]... SRC [SRC]... DEST
+
+    General function to push or pull a directory from a remote server to a local path
 
     References:
         http://www.tecmint.com/rsync-local-remote-file-synchronization-commands/
         http://serverfault.com/questions/219013/showing-total-progress-in-rsync-is-it-possible
     """
-    remote_path = os.path.join(remote_dpath, dname).replace('\\', '/')
-    local_uri = os.path.join(local_dpath).replace('\\', '/')
-    remote_uri = host + ':' + remote_path
-
-    if ut.WIN32:
-        # fix for mingw rsync
-        win32_drive, _path = os.path.splitdrive(local_uri)
-        mingw_drive = '/' + win32_drive[:-1].lower()
-        local_uri = mingw_drive + _path
-
     rsync_exe = 'rsync'
     #-v : verbose
     #-r : copies data recursively (but dont preserve timestamps and permission while transferring data
-    #-a : archive mode, archive mode allows copying files recursively and it also preserves symlinks, permissions, user and group ownerships, and timestamps
+    #-a : archive mode, allows recursive copying and preserves symlinks, permissions, user and group ownerships, and timestamps
     #-z : compress file data
     #-h : human-readable, output numbers in a human-readable format
     #-P                          same as --partial --progress
-    rsync_options = '-vrazhP'
+    rsync_options = '-vazhP'
     if len(exclude_dirs) > 0:
         exclude_tup = ['--exclude ' + dir_ for dir_ in exclude_dirs]
         exclude_opts = ' '.join(exclude_tup)
         rsync_options += ' ' + exclude_opts
 
-    cmdtuple = (rsync_exe, rsync_options, remote_uri, local_uri)
+    cmdtuple = (rsync_exe, rsync_options, src_uri, dst_uri)
     cmdstr = ' '.join(cmdtuple)
-    print('remote_uri = %r ' % (remote_uri,))
-    print('local_uri  = %r ' % (local_uri,))
+    print('src_uri = %r ' % (src_uri,))
+    print('dst_uri = %r ' % (dst_uri,))
     print('cmdstr = %r' % cmdstr)
 
     if not dryrun:
         ut.cmd(cmdstr)
 
 
-def rsync_pull_ibeisdb(host, dbname):
+def sync_ibeisdb(remote_uri, dbname, mode='pull'):
     """
     syncs an ibeisdb without syncing the cache or the chip directory
     (or the top level image directory because it shouldnt exist unless it is an
     old hots database)
     """
+    print('[sync_ibeisdb] Syncing dbname=%r with remote_uri=%r mode=%r' % (dbname, remote_uri, mode))
     import ibeis
-    # remote workdir
-    remote_dpath = '/raid/work'
     # localworkdir
-    local_dpath = ibeis.sysres.get_workdir().replace('\\', '/')
     exclude_dirs = [
-        '_ibsdb/_ibeis_cache',
-        '_ibsdb/chips',
+        ut.ensure_unixslash(ibeis.const.REL_PATHS.chips),
+        ut.ensure_unixslash(ibeis.const.REL_PATHS.cache),
+        #'_ibsdb/_ibeis_cache',
+        #'_ibsdb/chips',
         './images',
     ]
-    dname = dbname
-    rsync_pull(host, remote_dpath, local_dpath, dname, exclude_dirs)
-    #--exclude 'dir1'
+    local_uri = ut.ensure_unixslash(ibeis.sysres.get_workdir())
+    if ut.WIN32:
+        # fix for mingw rsync
+        local_uri = ut.ensure_mingw_drive(local_uri)
+    if mode == 'pull':
+        # pull remote to local
+        rsync(ut.unixjoin(remote_uri, dbname), local_uri, exclude_dirs)
+    elif mode == 'push':
+        # push local to remote
+        rsync(ut.unixjoin(local_uri, dbname), remote_uri, exclude_dirs)
+    else:
+        raise AssertionError('unknown mode=%r' % (mode,))
 
 
-def pull_pz_mugu_19():
-    host = 'joncrall@hyrule.cs.rpi.edu'
+def sync_pz_mugu_19(mode='pull'):
+    remote_uri = 'joncrall@hyrule.cs.rpi.edu:/raid/work'
     dbname = 'PZ_MUGU_19'
-    rsync_pull_ibeisdb(host, dbname)
+    sync_ibeisdb(remote_uri, dbname, mode)
 
 
 if __name__ == '__main__':
-    pull_pz_mugu_19()
+    """
+    CommandLine:
+        ib
+        python _scripts/rsync_ibeisdb.py push
+        python _scripts/rsync_ibeisdb.py pull --dryrun
+        python _scripts/rsync_ibeisdb.py push --dryrun
+    """
+    import sys
+    if len(sys.argv) < 2:
+        print('Usage: rsync_ibeisdb.py [push, pull]')
+        sys.exit(1)
+    mode = sys.argv[1]
+    sync_pz_mugu_19(mode=mode)
