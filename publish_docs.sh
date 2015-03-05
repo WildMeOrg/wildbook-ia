f3293b8f75d4e9187c39537d21822bc94d334376
joncrall <erotemic@gmail.com>
2015-03-05 16:18:19 -0500

    Updated docs

--- publish_docs.sh
+++ publish_docs.sh
@@ -11,8 +11,8 @@ git add _page/.nojekyll
 git add _page/*
 #git add _page
 git commit -m "updated docs"
-git subtree add --prefix _page origin gh-pages 
-git subtree pull --prefix _page origin gh-pages
+#git subtree add --prefix _page origin gh-pages 
+#git subtree pull --prefix _page origin gh-pages
 git subtree push --prefix _page origin gh-pages
 
 #git push origin --delete gh-pages
