679ff885b0c7575b04c57d232aca6b5f0a8ff7a7
joncrall <erotemic@gmail.com>
2015-05-21 15:24:11 -0400

    wip

--- ibeis/guitool/api_item_model.py
+++ ibeis/guitool/api_item_model.py
@@ -280,8 +280,9 @@ class APIItemModel(API_MODEL_BASE):
             #ut.embed()
             # <NUMPY MULTIARRAY SORT>
             with ut.embed_on_exception_context:
-                values = np.array(values)
                 if type_ is float:
+                    values = np.array(ut.replace_nones(values, np.nan))
+                    #values = np.array(values)
                     values[np.isnan(values)] = -np.inf  # Force nan to be the smallest number
                 sortx = vt.argsort_multiarray([values, id_list], reverse=reverse)
                 #sorting_records = np.rec.fromarrays([values, id_list])
