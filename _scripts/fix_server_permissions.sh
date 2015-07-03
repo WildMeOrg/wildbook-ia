3770fee649063aba65d6c3a88ff9dfd395c77433
joncrall <erotemic@gmail.com>
2015-07-03 16:31:57 -0400

    wip

--- _scripts/fix_server_permissions.sh
+++ _scripts/fix_server_permissions.sh
@@ -1,3 +1,9 @@
 cd /opt
 sudo chown -R root:ibeis ibeis
 sudo chmod -R g+w ibeis
+
+sudo chown -R root:ibeis "$(git rev-parse --show-toplevel)/.git"
+sudo chmod -R g+w "$(git rev-parse --show-toplevel)/.git"
+sudo chmod -R g+w *
+sudo chown -R root:ibeis *
+$USER:$USER 
