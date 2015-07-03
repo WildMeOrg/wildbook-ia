31f276fa58bd4bc7be38122c9e294058b9c879eb
joncrall <erotemic@gmail.com>
2015-07-03 16:36:57 -0400

    wip

--- ibeis/vtool/__init__.py
+++ ibeis/vtool/__init__.py
@@ -4,7 +4,7 @@ VTool - Computer vision tools
 """
 # flake8: noqa
 from __future__ import absolute_import, division, print_function
-__version__ = '1.0.0.dev1'
+__version__ = '1.0.1.dev1'
 
 import utool as ut
 ut.noinject(__name__, '[vtool]')
