6b939b4cb7141a0a8b52e4fcdb66b58347450996
Jason Parham <bluemellophone@gmail.com>
2015-07-15 14:59:11 -0400

    temp fix

--- ibeis/web/app.py
+++ ibeis/web/app.py
@@ -1261,7 +1261,7 @@ def image_src_api(gid=None):
     return image_src(gid)
 
 
-@register_api('/api/image/', methods=['POST'])
+@register_api('/api/image/', methods=['POST', 'OPTIONS'])
 def image_upload(**kwargs):
     r"""
     Returns the gid_list for image files submitted in a ZIP archive.  The image
