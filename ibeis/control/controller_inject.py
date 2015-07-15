9dab16ad2f07f359f7e0dfd603579722e2326fd3
Jason Parham <bluemellophone@gmail.com>
2015-07-15 12:43:40 -0400

    Turned off all cross-site scripting controls

--- ibeis/control/controller_inject.py
+++ ibeis/control/controller_inject.py
@@ -3,6 +3,8 @@ import utool as ut
 #import sys
 import flask
 from flask import current_app, request, make_response
+from datetime import timedelta
+from functools import update_wrapper
 from functools import wraps
 from os.path import abspath, join, dirname
 # import simplejson as json
@@ -232,6 +234,48 @@ def authentication_either(func):
     return wrp_authentication_either
 
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
 def get_ibeis_flask_api(__name__, DEBUG_PYTHON_STACK_TRACE_JSON_RESPONSE=True):
     if __name__ == '__main__':
         return ut.dummy_args_decor
@@ -244,6 +288,7 @@ def get_ibeis_flask_api(__name__, DEBUG_PYTHON_STACK_TRACE_JSON_RESPONSE=True):
                 app = get_flask_app()
                 @app.route(rule, **options)
                 @authentication_either
+                @crossdomain
                 @wraps(func)
                 def translated_call(*args, **kwargs):
                     #from flask import make_response
@@ -292,6 +337,7 @@ def get_ibeis_flask_route(__name__):
                 app = get_flask_app()
                 @app.route(rule, **options)
                 # @authentication_user_only
+                @crossdomain
                 @wraps(func)
                 def translated_call(*args, **kwargs):
                     try:
