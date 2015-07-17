7e5c3890fd6513c07ae9fa286d68ebcb89751bea
Jason Parham <bluemellophone@gmail.com>
2015-07-17 12:31:51 -0400

    NONE mapping fixes for CSV output

--- ibeis/ibsfuncs.py
+++ ibeis/ibsfuncs.py
@@ -3658,7 +3658,7 @@ def report_sightings(ibs, complete=True, include_images=False, **kwargs):
     def sanitize_list(data_list):
         data_list = [ str(data).replace(',', '<COMMA>') for data in list(data_list) ]
         return_str = (','.join(data_list))
-        return_str = return_str.replace(',None,', ',UNKNOWN,')
+        return_str = return_str.replace(',None,', ',NONE,')
         return_str = return_str.replace(',%s,' % (const.UNKNOWN, ) , ',UNKNOWN,')
         return_str = return_str.replace(',-1,', ',UNKNOWN,')
         return_str = return_str.replace(',-1,', ',UNKNOWN,')
