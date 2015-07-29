5da87211f053d4e003aa8c71d88e997ce5877ac0
joncrall <erotemic@gmail.com>
2015-07-29 12:17:12 -0400

    wip

--- installers.py
+++ installers.py
@@ -30,16 +30,166 @@ Win32CommandLine:
 """
 from __future__ import absolute_import, division, print_function
 from os.path import dirname, realpath, join, exists, normpath
-import six
+#import six
 import utool as ut
 import sys
 import importlib
 from os.path import join  # NOQA
 
 
+#def fix_command_tuple(command_tuple, sudo=False, shell=False, win32=ut.WIN32):
+#    r"""
+#    Args:
+#        command_tuple (?):
+#        sudo (bool):
+#        shell (bool):
+#
+#    Returns:
+#        tuple: (None, None, None)
+#
+#    CommandLine:
+#        python -m utool.util_cplat --test-fix_command_tuple:0
+#        python -m utool.util_cplat --test-fix_command_tuple:1
+#
+#    Example0:
+#        >>> # DISABLE_DOCTEST
+#        >>> from utool.util_cplat import *  # NOQA
+#        >>> command_tuple = ('pyinstaller', '_installers/pyinstaller-ibeis.spec') #, '-y'
+#        >>> result = fix_command_tuple(command_tuple)
+#        >>> print(result)
+#
+#    Example1:
+#        >>> # DISABLE_DOCTEST
+#        >>> from utool.util_cplat import *  # NOQA
+#        >>> command_tuple = 'pyinstaller --runtime-hook rthook_pyqt4.py _installers/pyinstaller-ibeis.spec -y'
+#        >>> result = fix_command_tuple(command_tuple)
+#        >>> print(result)
+#    """
+#    args = command_tuple
+#    print(type(args))
+#    print(args)
+#    if shell:
+#        # Popen only accepts strings is shell is True, which
+#        # it really shouldn't be.
+#        if  isinstance(args, (list, tuple)) and len(args) > 1:
+#            # Input is ['cmd', 'arg1', 'arg2']
+#            args = ' '.join(args)
+#        elif isinstance(args, (list, tuple)) and len(args) == 1:
+#            if isinstance(args[0], (tuple, list)):
+#                # input got nexted
+#                args = ' '.join(args)
+#            elif isinstance(args[0], six.string_types):
+#                # input is just nested string
+#                args = args[0]
+#        elif isinstance(args, six.string_types):
+#            pass
+#    if sudo is True:
+#        # On Windows it doesnt seem to matter if shlex splits the string or not
+#        # However on linux it seems like you need to split the string if you are
+#        # not using sudo, but if you use sudo you cannot split the string
+#        if not win32:
+#            if isinstance(args, six.string_types):
+#                import shlex
+#                args = shlex.split(args)
+#            args = ['sudo'] + args
+#            args = ' '.join(args)
+#        else:
+#            # TODO: strip out sudos
+#            pass
+#    return args
+
+
+#def system_command(command_tuple, detatch=False, sudo=False, shell=False, verbose=True):
+#    """
+#    Version 2 of util_cplat.cmd, hopefully it will work
+#
+#    Args:
+#        command_tuple (?):
+#        detatch (bool):
+#        sudo (bool):
+#        shell (bool):
+#        verbose (bool):  verbosity flag, shows process output if True
+#
+#    Returns:
+#        tuple: (None, None, None)
+#
+#    CommandLine:
+#        python -m utool.util_cplat --test-system_command
+#
+#    Example:
+#        >>> # DISABLE_DOCTEST
+#        >>> from utool.util_cplat import *  # NOQA
+#        >>> # build test data
+#        >>> command_tuple = ('pyinstaller', '_installers/pyinstaller-ibeis.spec') #, '-y'
+#        >>> detatch = False
+#        >>> sudo = False
+#        >>> shell = False
+#        >>> verbose = True
+#        >>> # execute function
+#        >>> system_command(command_tuple, detatch, sudo, shell, verbose)
+#        >>> # verify results
+#        >>> result = str((None, None, None))
+#        >>> print(result)
+#    """
+#    sys.stdout.flush()
+#    try:
+#        # Parse the keyword arguments
+#        # Do fancy things with args
+#        # Print what you are about to do
+#        args = fix_command_tuple(command_tuple, sudo, shell)
+#        print('[ut.cmd] RUNNING: %r' % (args,))
+#        # Open a subprocess with a pipe
+#        import subprocess
+#        proc = subprocess.Popen(args,
+#                                stdout=subprocess.PIPE,
+#                                stderr=subprocess.STDOUT,
+#                                shell=shell)
+#        if detatch:
+#            print('[ut.cmd] PROCESS DETATCHING')
+#            return None, None, 1
+#        if verbose and not detatch:
+#            print('[ut.cmd] RUNNING WITH VERBOSE OUTPUT')
+#            logged_out = []
+#            def _run_process(proc):
+#                while True:
+#                    # returns None while subprocess is running
+#                    retcode = proc.poll()
+#                    line = proc.stdout.readline()
+#                    yield line
+#                    if retcode is not None:
+#                        raise StopIteration('process finished')
+#            for line in _run_process(proc):
+#                line_ = line if six.PY2 else line.decode('utf-8')
+#                sys.stdout.write(line_)
+#                sys.stdout.flush()
+#                logged_out.append(line)
+#            out = '\n'.join(logged_out)
+#            (out_, err) = proc.communicate()
+#            #print('[ut.cmd] out: %s' % (out,))
+#            print('[ut.cmd] stdout: %s' % (out_,))
+#            print('[ut.cmd] stderr: %s' % (err,))
+#        else:
+#            # Surpress output
+#            #print('[ut.cmd] RUNNING WITH SUPRESSED OUTPUT')
+#            (out, err) = proc.communicate()
+#        # Make sure process if finished
+#        ret = proc.wait()
+#        print('[ut.cmd] PROCESS FINISHED')
+#        return out, err, ret
+#    except Exception as ex:
+#        import utool as ut
+#        if isinstance(args, tuple):
+#            print(ut.truepath(args[0]))
+#        elif isinstance(args, six.string_types):
+#            print(ut.unixpath(args))
+#        ut.printex(ex, 'Exception running ut.cmd',
+#                   keys=['verbose', 'detatch', 'shell', 'sudo'],
+#                   tb=True)
+
+
 def fix_pyinstaller_sip_api():
     """
-    Hack to get the correct version of SIP
+    Hack to get the correct version of SIP for win32
 
     References:
         http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
@@ -71,156 +221,6 @@ def fix_pyinstaller_sip_api():
     ut.inject_python_code(fpath, patch_code, tag)
     #ut.editfile(hook_fpath)
     pass
-
-
-def fix_command_tuple(command_tuple, sudo=False, shell=False, win32=ut.WIN32):
-    r"""
-    Args:
-        command_tuple (?):
-        sudo (bool):
-        shell (bool):
-
-    Returns:
-        tuple: (None, None, None)
-
-    CommandLine:
-        python -m utool.util_cplat --test-fix_command_tuple:0
-        python -m utool.util_cplat --test-fix_command_tuple:1
-
-    Example0:
-        >>> # DISABLE_DOCTEST
-        >>> from utool.util_cplat import *  # NOQA
-        >>> command_tuple = ('pyinstaller', '_installers/pyinstaller-ibeis.spec') #, '-y'
-        >>> result = fix_command_tuple(command_tuple)
-        >>> print(result)
-
-    Example1:
-        >>> # DISABLE_DOCTEST
-        >>> from utool.util_cplat import *  # NOQA
-        >>> command_tuple = 'pyinstaller --runtime-hook rthook_pyqt4.py _installers/pyinstaller-ibeis.spec -y'
-        >>> result = fix_command_tuple(command_tuple)
-        >>> print(result)
-    """
-    args = command_tuple
-    print(type(args))
-    print(args)
-    if shell:
-        # Popen only accepts strings is shell is True, which
-        # it really shouldn't be.
-        if  isinstance(args, (list, tuple)) and len(args) > 1:
-            # Input is ['cmd', 'arg1', 'arg2']
-            args = ' '.join(args)
-        elif isinstance(args, (list, tuple)) and len(args) == 1:
-            if isinstance(args[0], (tuple, list)):
-                # input got nexted
-                args = ' '.join(args)
-            elif isinstance(args[0], six.string_types):
-                # input is just nested string
-                args = args[0]
-        elif isinstance(args, six.string_types):
-            pass
-    if sudo is True:
-        # On Windows it doesnt seem to matter if shlex splits the string or not
-        # However on linux it seems like you need to split the string if you are
-        # not using sudo, but if you use sudo you cannot split the string
-        if not win32:
-            if isinstance(args, six.string_types):
-                import shlex
-                args = shlex.split(args)
-            args = ['sudo'] + args
-            args = ' '.join(args)
-        else:
-            # TODO: strip out sudos
-            pass
-    return args
-
-
-def system_command(command_tuple, detatch=False, sudo=False, shell=False, verbose=True):
-    """
-    Version 2 of util_cplat.cmd, hopefully it will work
-
-    Args:
-        command_tuple (?):
-        detatch (bool):
-        sudo (bool):
-        shell (bool):
-        verbose (bool):  verbosity flag, shows process output if True
-
-    Returns:
-        tuple: (None, None, None)
-
-    CommandLine:
-        python -m utool.util_cplat --test-system_command
-
-    Example:
-        >>> # DISABLE_DOCTEST
-        >>> from utool.util_cplat import *  # NOQA
-        >>> # build test data
-        >>> command_tuple = ('pyinstaller', '_installers/pyinstaller-ibeis.spec') #, '-y'
-        >>> detatch = False
-        >>> sudo = False
-        >>> shell = False
-        >>> verbose = True
-        >>> # execute function
-        >>> system_command(command_tuple, detatch, sudo, shell, verbose)
-        >>> # verify results
-        >>> result = str((None, None, None))
-        >>> print(result)
-    """
-    sys.stdout.flush()
-    try:
-        # Parse the keyword arguments
-        # Do fancy things with args
-        # Print what you are about to do
-        args = fix_command_tuple(command_tuple, sudo, shell)
-        print('[ut.cmd] RUNNING: %r' % (args,))
-        # Open a subprocess with a pipe
-        import subprocess
-        proc = subprocess.Popen(args,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                shell=shell)
-        if detatch:
-            print('[ut.cmd] PROCESS DETATCHING')
-            return None, None, 1
-        if verbose and not detatch:
-            print('[ut.cmd] RUNNING WITH VERBOSE OUTPUT')
-            logged_out = []
-            def _run_process(proc):
-                while True:
-                    # returns None while subprocess is running
-                    retcode = proc.poll()
-                    line = proc.stdout.readline()
-                    yield line
-                    if retcode is not None:
-                        raise StopIteration('process finished')
-            for line in _run_process(proc):
-                line_ = line if six.PY2 else line.decode('utf-8')
-                sys.stdout.write(line_)
-                sys.stdout.flush()
-                logged_out.append(line)
-            out = '\n'.join(logged_out)
-            (out_, err) = proc.communicate()
-            #print('[ut.cmd] out: %s' % (out,))
-            print('[ut.cmd] stdout: %s' % (out_,))
-            print('[ut.cmd] stderr: %s' % (err,))
-        else:
-            # Surpress output
-            #print('[ut.cmd] RUNNING WITH SUPRESSED OUTPUT')
-            (out, err) = proc.communicate()
-        # Make sure process if finished
-        ret = proc.wait()
-        print('[ut.cmd] PROCESS FINISHED')
-        return out, err, ret
-    except Exception as ex:
-        import utool as ut
-        if isinstance(args, tuple):
-            print(ut.truepath(args[0]))
-        elif isinstance(args, six.string_types):
-            print(ut.unixpath(args))
-        ut.printex(ex, 'Exception running ut.cmd',
-                   keys=['verbose', 'detatch', 'shell', 'sudo'],
-                   tb=True)
 
 
 def get_setup_dpath():
@@ -318,7 +318,7 @@ def ensure_inno_isinstalled():
 
 
 def ensure_inno_script():
-    """ writes inno script to distk """
+    """ writes inno script to disk for win32 installer build """
     cwd = get_setup_dpath()
     iss_script_fpath = join(cwd, '_installers', 'win_installer_script.iss')
     # THE ISS USES {} AS SYNTAX. CAREFUL
--- main.py
+++ main.py
@@ -16,6 +16,24 @@ from __future__ import absolute_import, division, print_function
 import multiprocessing
 from ibeis.__main__ import run_ibeis
 
+
+def dependencies_for_myprogram():
+    """ Let pyintaller find these modules
+
+    References:
+        http://stackoverflow.com/questions/18596410/importerror-no-module-named-mpl-toolkits-with-maptlotlib-1-3-0-and-py2exe
+    """
+    from guitool.__PYQT__ import QtCore, QtGui  # Pyinstaller hacks  # NOQA
+    from PyQt4 import QtCore, QtGui  # NOQA
+    #from PyQt4 import QtCore, QtGui  # NOQA
+    from scipy.sparse.csgraph import _validation  # NOQA
+    from scipy.special import _ufuncs_cxx  # NOQA
+    from mpl_toolkits.axes_grid1 import make_axes_locatable  # NOQA
+    #import lru  # NOQA
+    # Workaround for mpl_toolkits
+    import importlib
+    importlib.import_module('mpl_toolkits').__path__
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()  # for win32
     run_ibeis()
