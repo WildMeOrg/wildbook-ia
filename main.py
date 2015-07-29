9030f26104c37fff1a3452ee88e1c57e6051f920
joncrall <erotemic@gmail.com>
2015-07-29 13:00:41 -0400

    wip

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ def dependencies_for_myprogram():
     References:
         http://stackoverflow.com/questions/18596410/importerror-no-module-named-mpl-toolkits-with-maptlotlib-1-3-0-and-py2exe
     """
-    from guitool.__PYQT__ import QtCore, QtGui  # Pyinstaller hacks  # NOQA
+    import PyQt4  # NOQA
     from PyQt4 import QtCore, QtGui  # NOQA
+    from guitool.__PYQT__ import QtCore, QtGui  # Pyinstaller hacks  # NOQA
     #from PyQt4 import QtCore, QtGui  # NOQA
     from scipy.sparse.csgraph import _validation  # NOQA
     from scipy.special import _ufuncs_cxx  # NOQA
+    import mpl_toolkits.axes_grid1  # NOQA
     from mpl_toolkits.axes_grid1 import make_axes_locatable  # NOQA
     #import lru  # NOQA
     # Workaround for mpl_toolkits
