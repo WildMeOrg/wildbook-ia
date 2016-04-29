b2d6de99899f58ecdc6e4f941e1f48b5d3a22d82
Jason Parham <bluemellophone@gmail.com>
2016-04-29 14:13:10 -0400

    Assert

--- ibeis/core_images.py
+++ ibeis/core_images.py
@@ -355,7 +355,7 @@ def compute_labels_localizations(depc, loc_id_list, config=None):
     """
     from ibeis.algo.detect.labeler.labeler import label_chip_list
     print('[ibs] Process Localization Labels (len = %d)' % (len(loc_id_list), ))
-    assert len(loc_id_list) <= 32
+    assert len(loc_id_list) <= 32, 'Got %d > 32 ids' % (len(loc_id_list), )
     print('config = %r' % (config,))
     # Get controller
     ibs = depc.controller
