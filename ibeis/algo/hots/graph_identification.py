9b63a32cc81b024970c39d1b9c8bc3c181505188
joncrall <erotemic@gmail.com>
2016-06-16 14:23:42 -0400

    Fixed threshold issue

--- ibeis/algo/hots/graph_identification.py
+++ ibeis/algo/hots/graph_identification.py
@@ -227,7 +227,7 @@ class AnnotInference(object):
         thresh = nscores[idx2]
         #print('thresh = %r' % (thresh,))
         #thresh = .999
-        thresh = .1
+        #thresh = .1
         return thresh
 
     def make_graph2(infr):
