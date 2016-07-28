8073de56a88f09b6e904fda750a803ef8e8ef68e
Jason Parham <bluemellophone@gmail.com>
2016-07-28 15:25:39 -0400

    Scoping issue

--- ibeis/web/job_engine.py
+++ ibeis/web/job_engine.py
@@ -370,7 +370,7 @@ class JobBackend(object):
 
         if self.spawn_queue:
             self.engine_queue_proc = _spawner(engine_queue_loop, self.port_dict)
-            self.collect_queue_proc = _spawner(make_queue_loop, self.port_dict)
+            self.collect_queue_proc = _spawner(collect_queue_loop, self.port_dict)
         if self.spawn_collector:
             self.collect_proc = _spawner(collector_loop, self.port_dict, dbdir)
         if self.spawn_engine:
@@ -541,7 +541,7 @@ class JobInterface(object):
                 raise Exception('Timeout')
 
 
-def make_queue_loop(port_dict, name='collect'):
+def make_queue_loop(name='collect'):
     """
     Standard queue loop
 
@@ -554,8 +554,8 @@ def make_queue_loop(port_dict, name='collect'):
     assert name is not None, 'must name queue'
     queue_name = name + '_queue'
     loop_name = queue_name + '_loop'
-    iface1, iface2 = port_dict['collect_url1'], port_dict['collect_url2']
-    def queue_loop():
+    def queue_loop(port_dict):
+        iface1, iface2 = port_dict['collect_url1'], port_dict['collect_url2']
         print = partial(ut.colorprint, color='green')
         update_proctitle(queue_name)
 
@@ -605,6 +605,9 @@ def make_queue_loop(port_dict, name='collect'):
                 print('Exiting %s' % (loop_name,))
     ut.set_funcname(queue_loop, loop_name)
     return queue_loop
+
+
+collect_queue_loop = make_queue_loop(name='collect')
 
 
 def engine_queue_loop(port_dict):
