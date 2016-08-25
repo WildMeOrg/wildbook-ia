bb177ebe28c258571b188909d8cd43a16a196046
joncrall <erotemic@gmail.com>
2016-08-25 17:50:07 -0400

    wip

--- ibeis/scripts/classify_shark.py
+++ ibeis/scripts/classify_shark.py
@@ -47,7 +47,7 @@ def shark_net(dry=False):
         #
         output_dims=2,
         data_shape=config['dim_size'] + (3,),
-        batch_size=16,
+        batch_size=64,
     )
     model.init_arch()
     model.print_layer_info()
