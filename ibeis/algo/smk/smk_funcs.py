Fixed bug in feature weighting

ibeis/algo/smk/smk_funcs.py
@@ -48,6 +48,11 @@ Notes:
     smk:nAssign=1,SV=True,: .58
     smk:nAssign=1,SV=False,: .38
 
+    Yesterday I got
+    .22 when I fixed the bounding boxes
+    And now I'm getting
+    .08 and .32 (sv=[F,T]) after deleting and redoing everything (also removing junk images)
+
 Differences Between this and SMK
    * No RootSIFT
    * No SIFT Centering
@@ -454,6 +459,9 @@ def sep_match_scores(phisX_list, phisY_list, flagsX_list, flagsY_list, alpha, th
 @profile
 def agg_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list):
     r"""
+    Builds explicit features matches. Break and distribute up each aggregate
+    score amongst its contributing features.
+
     Returns:
         tuple: (fm, fs)
 
@@ -473,16 +481,18 @@ def agg_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list):
         >>> (fm, fs) = agg_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list)
         >>> print('fm = ' + ut.repr2(fm))
         >>> print('fs = ' + ut.repr2(fs))
+        >>> assert len(fm) == len(fs)
+        >>> assert score_list.sum() == fs.sum()
     """
     # Build feature matches
     # Spread word score according to contriubtion (maw) weight
-    unflat_fs = [maws1[:, None].dot(maws2[:, None].T).ravel()
-                 for maws1, maws2 in zip(X_maws, Y_maws)]
-    factor_list = np.array([contrib.sum() for contrib in unflat_fs],
-                           dtype=np.float32)
-    factor_list = np.multiply(factor_list, score_list, out=factor_list)
-    for contrib, factor in zip(unflat_fs, factor_list):
-        np.multiply(contrib, factor, out=contrib)
+    unflat_contrib = [maws1[:, None].dot(maws2[:, None].T).ravel()
+                      for maws1, maws2 in zip(X_maws, Y_maws)]
+    unflat_factor = [contrib / contrib.sum() for contrib in unflat_contrib]
+    #factor_list = np.divide(score_list, factor_list, out=factor_list)
+    for score, factor in zip(score_list, unflat_factor):
+        np.multiply(score, factor, out=factor)
+    unflat_fs = unflat_factor
 
     # itertools.product seems fastest for small arrays
     unflat_fm = (ut.product(fxs1, fxs2)
@@ -497,18 +507,37 @@ def agg_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list):
 
 
 @profile
-def sep_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list):
-    # Spread word score according to contriubtion (maw) weight
-    unflat_weight = [maws1[:, None].dot(maws2[:, None].T).ravel()
-                     for maws1, maws2 in zip(X_maws, Y_maws)]
-    flat_weight = np.array(ut.flatten(unflat_weight), dtype=np.float32)
-    fs = np.array(ut.flatten(score_list), dtype=np.float32)
-    np.multiply(fs, flat_weight, out=fs)
+def sep_build_matches(X_fxs, Y_fxs, scores_list):
+    r"""
+    Just build matches. Scores have already been broken up. No need to do that.
 
-    # itertools.product seems fastest for small arrays
+    Returns:
+        tuple: (fm, fs)
+
+    CommandLine:
+        python -m ibeis.algo.smk.smk_funcs agg_build_matches --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from ibeis.algo.smk.smk_funcs import *  # NOQA
+        >>> map_int = ut.partial(ut.lmap, ut.partial(np.array, dtype=np.int32))
+        >>> map_float = ut.partial(ut.lmap, ut.partial(np.array, dtype=np.float32))
+        >>> X_fxs = map_int([[0, 1], [2, 3, 4], [5]])
+        >>> Y_fxs = map_int([[8], [0, 4], [99]])
+        >>> scores_list = map_float([
+        >>>     [[.1], [.2],],
+        >>>     [[.3, .4], [.4, .6], [.5, .9],],
+        >>>     [[.4]],
+        >>> ])
+        >>> (fm, fs) = sep_build_matches(X_fxs, Y_fxs, scores_list)
+        >>> print('fm = ' + ut.repr2(fm))
+        >>> print('fs = ' + ut.repr2(fs))
+        >>> assert len(fm) == len(fs)
+        >>> assert score_list.sum() == fs.sum()
+    """
+    fs = np.array(ut.total_flatten(scores_list), dtype=np.float32)
     unflat_fm = (ut.product(fxs1, fxs2)
                  for fxs1, fxs2 in zip(X_fxs, Y_fxs))
-
     fm = np.array(ut.flatten(unflat_fm), dtype=np.int32)
     isvalid = np.greater(fs, 0)
     fm = fm.compress(isvalid, axis=0)

ibeis/algo/smk/smk_pipeline.py
@@ -35,7 +35,7 @@ class SMKRequestConfig(dtool.Config):
         ut.ParamInfo('agg', True),
         ut.ParamInfo('data_ma', False),  # hack for query only multiple assignment
         ut.ParamInfo('word_weight_method', 'idf', 'wwm'),  # hack for query only multiple assignment
-        ut.ParamInfo('smk_version', 2),
+        ut.ParamInfo('smk_version', 3),
     ]
     _sub_config_list = [
         core_annots.ChipConfig,
@@ -405,13 +405,13 @@ class SMK(ut.NiceRepr):
             (score, score_list, X, Y, X_idx, Y_idx) = item
             X_fxs = ut.take(X.fxs_list, X_idx)
             Y_fxs = ut.take(Y.fxs_list, Y_idx)
-            X_maws = ut.take(X.maws_list, X_idx)
-            Y_maws = ut.take(Y.maws_list, Y_idx)
             # Only build matches for those that sver will use
             if agg:
+                X_maws = ut.take(X.maws_list, X_idx)
+                Y_maws = ut.take(Y.maws_list, Y_idx)
                 fm, fs = smk_funcs.agg_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list)
             else:
-                fm, fs = smk_funcs.sep_build_matches(X_fxs, Y_fxs, X_maws, Y_maws, score_list)
+                fm, fs = smk_funcs.sep_build_matches(X_fxs, Y_fxs, score_list)
             if len(fm) > 0:
                 #assert not np.any(np.isnan(fs))
                 daid = Y.aid

ibeis/expt/test_result.py
@@ -2369,19 +2369,14 @@ class TestResult(ut.NiceRepr):
             >>> import ibeis
             >>> ibs = ibeis.opendb('Oxford')
             >>> #ibs, testres = ibeis.testdata_expts('Oxford', a='oxford', p='smk:nWords=[64000],nAssign=[1],SV=[False,True]')
-            >>> ibs, testres = ibeis.testdata_expts('Oxford', a='oxford', p='smk:nWords=[64000],nAssign=[1],SV=[False,True]')
+            >>> ibs, testres = ibeis.testdata_expts('Oxford', a='oxford', p='smk:nWords=[64000],nAssign=[1],SV=[False,True],can_match_sameimg=True')
         """
         import sklearn.metrics
         qaids = testres.get_test_qaids()
         ibs = testres.ibs
 
-        PLOT = True
         PLOT = False
-
-        if PLOT:
-            import plottool as pt
-            pt.qt4ensure()
-            pt.figure()
+        PLOT = True
 
         cfgx2_cms = []
         for qreq_ in testres.cfgx2_qreq_:
@@ -2389,32 +2384,43 @@ class TestResult(ut.NiceRepr):
             cm_list = [cm.extend_results(qreq_) for cm in cm_list]
             for cm in cm_list:
                 cm.score_csum(qreq_)
-                #cm.sortself()
+            #    #cm.sortself()
             cfgx2_cms.append(cm_list)
 
         map_list = []
         unique_names, groupxs = ut.group_indices(ibs.annots(qaids).names)
 
-        for cm in cfgx2_cms:
-            avep_list = []
+        for cm_list, qreq_ in zip(cfgx2_cms, testres.cfgx2_qreq_):
+            if PLOT:
+                import plottool as pt
+                pt.qt4ensure()
+                fnum = pt.ensure_fnum(None)
+                pt.figure(fnum=fnum)
+                avep_list = []
             #fnum = pt.ensure_fnum(None)
             #pt.figure(fnum=fnum)
             for cm in cm_list:
                 # Ignore junk images
                 flags   = np.array(ibs.annots(cm.daid_list).quality_texts) != 'junk'
-                y_true  = (cm.qnid == cm.dnid_list).compress(flags)
+                assert np.all(flags)
+                daid_list = cm.daid_list
+                dnid_list = cm.dnid_list
+                y_true  = (cm.qnid == dnid_list).compress(flags).astype(np.int)
                 y_score = cm.annot_score_list.compress(flags)
 
                 y_score[~np.isfinite(y_score)] = 0
                 y_score = np.nan_to_num(y_score)
-                #sortx = np.argsort(y_score)
-                #y_true = y_
+                sortx = np.argsort(y_score)[::-1]
+                daid_list = daid_list.take(sortx)
+                dnid_list = dnid_list.take(sortx)
+                y_true = y_true.take(sortx)
+                y_score = y_score.take(sortx)
                 #print(cm.get_annot_ranks(cm.get_top_gt_aids(ibs)))
 
                 precision, recall, thresholds = sklearn.metrics.precision_recall_curve(y_true, y_score)
 
                 if PLOT:
-                    pt.plot(precision, recall)
+                    pt.plot2(recall, precision, marker='', linestyle='-', x_label='recall', y_label='precision')
 
                 avep = sklearn.metrics.average_precision_score(y_true, y_score)
                 #avep = [