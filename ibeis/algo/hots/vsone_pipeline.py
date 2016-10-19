a8b02fa3e861173882a7b6538817ba73b91f74a5
joncrall <crallj@rpi.edu>
2016-10-18 20:42:35 -0400

    wip

--- ibeis/algo/hots/vsone_pipeline.py
+++ ibeis/algo/hots/vsone_pipeline.py
@@ -85,15 +85,20 @@ def get_training_pairs():
             * Number of correspondences
             % Total LNBNN Score
 
+    CommandLine:
+        python -m ibeis.algo.hots.vsone_pipeline get_training_pairs
+
+    Example:
         >>> from ibeis.algo.hots.vsone_pipeline import *  # NOQA
         >>> from vtool.matching import *  # NOQA
+        >>> get_training_pairs()
     """
     import ibeis
     qreq_ = ibeis.testdata_qreq_(defaultdb='PZ_MTEST', a='default')
     cm_list = qreq_.execute()
-    num_gt = 3
-    num_hard_gf = 2
-    num_rand_gf = 1
+    num_gt = 4
+    num_hard_gf = 3
+    num_rand_gf = 2
     aid_pairs = []
 
     # Extract pairs of annotation ids
@@ -252,207 +257,82 @@ def get_training_pairs():
     X_nonan = pairwise_feats[valid_cols].values.copy()
 
     y = np.array([m.annot1['nid'] == m.annot2['nid'] for m in matches_RAT_SV])
-
+    # import utool
+    # utool.embed()
     rng = np.random.RandomState(42)
-    xvalkw = dict(n_splits=5, shuffle=True, random_state=rng)
-    skf = sklearn.model_selection.StratifiedKFold(**xvalkw)
-    skf_iter = skf.split(X=X_nonan, y=y)
-    df_results = pd.DataFrame(columns=['auc_naive', 'auc_learn_nonan', 'auc_learn_withnan'])
+    import sys
 
-    rf_params = dict(n_estimators=20, bootstrap=True, verbose=1)
-    for count, (train_idx, test_idx) in enumerate(skf_iter):
-        print('\n========')
-        y_test = y[test_idx]
-        y_train = y[train_idx]
-        if True:
-            score_list = np.array([m.fs.sum() for m in ut.take(matches_RAT_SV, test_idx)])
-            auc_naive = sklearn.metrics.roc_auc_score(y_test, score_list)
+    for seed in (rng.rand(5) * sys.maxint).astype(np.int):
+        print('seed = %r' % (seed,))
+        rng = np.random.RandomState(42)
+        xvalkw = dict(n_splits=10, shuffle=True, random_state=rng)
+        skf = sklearn.model_selection.StratifiedKFold(**xvalkw)
+        skf_iter = skf.split(X=X_nonan, y=y)
+        df_results = pd.DataFrame(columns=['auc_naive', 'auc_learn_nonan', 'auc_learn_withnan'])
 
-        if True:
-            X_train = X_nonan[train_idx]
-            X_test = X_nonan[test_idx]
-            # Train uncalibrated random forest classifier on train data
-            clf = RandomForestClassifier(**rf_params)
-            clf.fit(X_train, y_train)
+        rng2 = np.random.RandomState(seed)
+        # rf_params = dict(n_estimators=256, bootstrap=True, verbose=0, random_state=rng2)
+        rf_params = dict(n_estimators=256, bootstrap=False, verbose=0, random_state=rng2)
+        for count, (train_idx, test_idx) in enumerate(skf_iter):
+            y_test = y[test_idx]
+            y_train = y[train_idx]
+            if True:
+                score_list = np.array([m.fs.sum() for m in ut.take(matches_RAT_SV, test_idx)])
+                auc_naive = sklearn.metrics.roc_auc_score(y_test, score_list)
 
-            # evaluate on test data
-            clf_probs = clf.predict_proba(X_test)
-            auc_learn_nonan = sklearn.metrics.roc_auc_score(y_test, clf_probs.T[1])
+            if True:
+                X_train = X_nonan[train_idx]
+                X_test = X_nonan[test_idx]
+                # Train uncalibrated random forest classifier on train data
+                clf = RandomForestClassifier(**rf_params)
+                clf.fit(X_train, y_train)
 
-        if allow_nan:
-            X_train = X_withnan[train_idx]
-            X_test = X_withnan[test_idx]
-            # Train uncalibrated random forest classifier on train data
-            clf = RandomForestClassifier(missing_values=np.nan, **rf_params)
-            clf.fit(X_train, y_train)
+                # evaluate on test data
+                clf_probs = clf.predict_proba(X_test)
+                auc_learn_nonan = sklearn.metrics.roc_auc_score(y_test, clf_probs.T[1])
 
-            # evaluate on test data
-            clf_probs = clf.predict_proba(X_test)
-            # log_loss = sklearn.metrics.log_loss(y_test, clf_probs)
-            auc_learn_withnan = sklearn.metrics.roc_auc_score(y_test, clf_probs.T[1])
+            if allow_nan:
+                X_train = X_withnan[train_idx]
+                X_test = X_withnan[test_idx]
+                # Train uncalibrated random forest classifier on train data
+                clf = RandomForestClassifier(missing_values=np.nan, **rf_params)
+                clf.fit(X_train, y_train)
 
-        newrow = pd.DataFrame([[auc_naive, auc_learn_nonan, auc_learn_withnan]], columns=df_results.columns)
-        print(newrow)
-        df_results = df_results.append([newrow], ignore_index=True)
+                # evaluate on test data
+                clf_probs = clf.predict_proba(X_test)
+                # log_loss = sklearn.metrics.log_loss(y_test, clf_probs)
+                auc_learn_withnan = sklearn.metrics.roc_auc_score(y_test, clf_probs.T[1])
 
-    print(df_results)
+            newrow = pd.DataFrame([[auc_naive, auc_learn_nonan, auc_learn_withnan]], columns=df_results.columns)
+            # print(newrow)
+            df_results = df_results.append([newrow], ignore_index=True)
 
-    # TODO: TSNE?
-    # http://scikit-learn.org/stable/auto_examples/manifold/plot_manifold_sphere.html#sphx-glr-auto-examples-manifold-plot-manifold-sphere-py
-    # Perform t-distributed stochastic neighbor embedding.
-    from sklearn import manifold
-    import matplotlib.pyplot as plt
-    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
-    trans_data = tsne.fit_transform(feats).T
-    ax = fig.add_subplot(2, 5, 10)
-    plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
-    plt.title("t-SNE (%.2g sec)" % (t1 - t0))
-    ax.xaxis.set_major_formatter(NullFormatter())
-    ax.yaxis.set_major_formatter(NullFormatter())
-    plt.axis('tight')
+        # print(df_results)
 
-    def monkey_to_str_columns(self):
-        frame = self.tr_frame
-        highlight_func = 'max'
-        highlight_func = ut.partial(np.argmax, axis=1)
-        highlight_cols = [0, 1]
+        # TODO: TSNE?
+        # http://scikit-learn.org/stable/auto_examples/manifold/plot_manifold_sphere.html#sphx-glr-auto-examples-manifold-plot-manifold-sphere-py
+        # Perform t-distributed stochastic neighbor embedding.
+        # from sklearn import manifold
+        # import matplotlib.pyplot as plt
+        # tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+        # trans_data = tsne.fit_transform(feats).T
+        # ax = fig.add_subplot(2, 5, 10)
+        # plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+        # plt.title("t-SNE (%.2g sec)" % (t1 - t0))
+        # ax.xaxis.set_major_formatter(NullFormatter())
+        # ax.yaxis.set_major_formatter(NullFormatter())
+        # plt.axis('tight')
+        print('--------')
 
-        perrow_colxs = highlight_func(frame[highlight_cols].values)
-        n_rows = len(perrow_colxs)
-        n_cols = len(highlight_cols)
-        shape = (n_rows, n_cols)
-        flat_idxs = np.ravel_multi_index((np.arange(n_rows), perrow_colxs), shape)
-        flags2d = np.zeros(shape, dtype=np.int32)
-        flags2d.ravel()[flat_idxs] = 1
-        # np.unravel_index(flat_idxs, shape)
+        df = df_results
+        change = df[df.columns[2]] - df[df.columns[0]]
+        percent_change = change / df[df.columns[0]] * 100
+        df = df.assign(change=change)
+        df = df.assign(percent_change=percent_change)
 
-        def color_func(val, level):
-            if level:
-                return ut.color_text(val, 'red')
-            else:
-                return val
-
-        _make_fixed_width = pd.formats.format._make_fixed_width
-        frame = self.tr_frame
-        str_index = self._get_formatted_index(frame)
-        str_columns = self._get_formatted_column_labels(frame)
-        if self.header:
-            stringified = []
-            for i, c in enumerate(frame):
-                cheader = str_columns[i]
-                max_colwidth = max(self.col_space or 0, *(self.adj.len(x)
-                                                          for x in cheader))
-                fmt_values = self._format_col(i)
-                fmt_values = _make_fixed_width(fmt_values, self.justify,
-                                               minimum=max_colwidth,
-                                               adj=self.adj)
-                max_len = max(np.max([self.adj.len(x) for x in fmt_values]),
-                              max_colwidth)
-                cheader = self.adj.justify(cheader, max_len, mode=self.justify)
-
-                # Apply custom coloring
-                # cflags = flags2d.T[i]
-                # fmt_values = [color_func(val, level) for val, level in zip(fmt_values, cflags)]
-
-                stringified.append(cheader + fmt_values)
-        else:
-            stringified = []
-            for i, c in enumerate(frame):
-                fmt_values = self._format_col(i)
-                fmt_values = _make_fixed_width(fmt_values, self.justify,
-                                               minimum=(self.col_space or 0),
-                                               adj=self.adj)
-
-                stringified.append(fmt_values)
-
-        strcols = stringified
-        if self.index:
-            strcols.insert(0, str_index)
-
-        # Add ... to signal truncated
-        truncate_h = self.truncate_h
-        truncate_v = self.truncate_v
-
-        if truncate_h:
-            col_num = self.tr_col_num
-            # infer from column header
-            col_width = self.adj.len(strcols[self.tr_size_col][0])
-            strcols.insert(self.tr_col_num + 1, ['...'.center(col_width)] *
-                           (len(str_index)))
-        if truncate_v:
-            n_header_rows = len(str_index) - len(frame)
-            row_num = self.tr_row_num
-            for ix, col in enumerate(strcols):
-                # infer from above row
-                cwidth = self.adj.len(strcols[ix][row_num])
-                is_dot_col = False
-                if truncate_h:
-                    is_dot_col = ix == col_num + 1
-                if cwidth > 3 or is_dot_col:
-                    my_str = '...'
-                else:
-                    my_str = '..'
-
-                if ix == 0:
-                    dot_mode = 'left'
-                elif is_dot_col:
-                    cwidth = self.adj.len(strcols[self.tr_size_col][0])
-                    dot_mode = 'center'
-                else:
-                    dot_mode = 'right'
-                dot_str = self.adj.justify([my_str], cwidth, mode=dot_mode)[0]
-                strcols[ix].insert(row_num + n_header_rows, dot_str)
-
-        for cx_ in highlight_cols:
-            cx = cx_ + bool(self.header)
-            col = strcols[cx]
-            for rx, val in enumerate(col[1:], start=1):
-                strcols[cx][rx] = color_func(val, flags2d[rx - 1, cx - 1])
-
-        return strcols
-
-    def to_string_monkey(df):
-        """  monkey patch to pandas to highlight the maximum value in specified
-        cols of a row """
-        kwds = dict(buf=None, columns=None, col_space=None, header=True,
-                    index=True, na_rep='NaN', formatters=None,
-                    float_format=None, sparsify=None, index_names=True,
-                    justify=None, line_width=None, max_rows=None,
-                    max_cols=None, show_dimensions=False)
-        self = pd.formats.format.DataFrameFormatter(df, **kwds)
-        ut.inject_func_as_method(self, monkey_to_str_columns, '_to_str_columns', override=True, force=True)
-        def strip_ansi(text):
-            import re
-            ansi_escape = re.compile(r'\x1b[^m]*m')
-            return ansi_escape.sub('', text)
-        def justify_ansi(self, texts, max_len, mode='right'):
-            if mode == 'left':
-                return [x.ljust(max_len + (len(x) - len(strip_ansi(x)))) for x in texts]
-            elif mode == 'center':
-                return [x.center(max_len + (len(x) - len(strip_ansi(x)))) for x in texts]
-            else:
-                return [x.rjust(max_len + (len(x) - len(strip_ansi(x)))) for x in texts]
-        def strlen_ansii(self, text):
-            return pd.compat.strlen(strip_ansi(text), encoding=self.encoding)
-        ut.inject_func_as_method(self.adj, strlen_ansii, 'len', override=True, force=True)
-        ut.inject_func_as_method(self.adj, justify_ansi, 'justify', override=True, force=True)
-        # strcols = monkey_to_str_columns(self)
-        # texts = strcols[2]
-        # str_ = self.adj.adjoin(1, *strcols)
-        # print(str_)
-        # print(strip_ansi(str_))
-        self.to_string()
-        result = self.buf.getvalue()
-        return result
-
-    df = df_results
-    change = df[df.columns[1]] - df[df.columns[0]]
-    percent_change = change / df[df.columns[0]] * 100
-    df = df.assign(change=change)
-    df = df.assign(percent_change=percent_change)
-
-    print(to_string_monkey(df))
-    print(df.to_string())
+        import sandbox_utools as sbut
+        print(sbut.to_string_monkey(df, highlight_cols=[0, 1, 2]))
+    # print(df.to_string())
 
 
 def build_vsone_metadata(qaid, daid, qreq_, use_ibscache=True):
