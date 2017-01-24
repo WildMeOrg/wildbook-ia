86fa068ca849ff5d92626ebac244b97c9aec8d20
Jason Parham <bluemellophone@gmail.com>
2017-01-24 12:38:23 -0500

    Add selective search wrapper

---
+++ ibeis/algo/detect/selectivesearch.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Interface to Selective Search object proposals.
+"""
+from __future__ import absolute_import, division, print_function
+import utool as ut
+import vtool as vt
+from six.moves import zip
+import tempfile
+import subprocess
+import shlex
+import os
+from os.path import abspath, dirname, expanduser, join, exists  # NOQA
+import numpy as np
+import scipy.io
+(print, rrr, profile) = ut.inject2(__name__, '[selective search]')
+
+# SCRIPT_PATH = abspath(dirname(__file__))
+SCRIPT_PATH = abspath(expanduser(join('~', 'code', 'selective_search_ijcv_with_python')))
+
+if not ut.get_argflag('--no-selective-search'):
+    try:
+        assert exists(SCRIPT_PATH)
+    except AssertionError as ex:
+        if ut.SUPER_STRICT:
+            print('WARNING Failed to find selective search. '
+                  'Selective Search is unavailable')
+            raise
+
+
+VERBOSE_SS = ut.get_argflag('--verbdss') or ut.VERBOSE
+
+
+def detect_gid_list(ibs, gid_list, downsample=False, **kwargs):
+    """
+    Args:
+        gid_list (list of int): the list of IBEIS image_rowids that need detection
+        downsample (bool, optional): a flag to indicate if the original image
+            sizes should be used; defaults to True
+
+            True:  ibs.get_image_detectpaths() is used
+            False: ibs.get_image_paths() is used
+
+    Kwargs (optional): refer to the Selective Search documentation for configuration settings
+
+    Args:
+        ibs (ibeis.IBEISController):  image analysis api
+        gid_list (list of int): the list of IBEIS image_rowids that need detection
+        downsample (bool, optional): a flag to indicate if the original image
+                sizes should be used; defaults to True
+
+    Kwargs:
+        detector, config_filepath, weights_filepath, verbose
+
+    Yields:
+        tuple: (gid, gpath, result_list)
+
+    CommandLine:
+        python -m ibeis.algo.detect.yolo detect_gid_list --show
+
+    Example:
+        >>> # DISABLE_DOCTEST
+        >>> from ibeis.algo.detect.yolo import *  # NOQA
+        >>> from ibeis.core_images import LocalizerConfig
+        >>> import ibeis
+        >>> ibs = ibeis.opendb('testdb1')
+        >>> gid_list = ibs.get_valid_gids()
+        >>> kwargs = config = LocalizerConfig(**{
+        >>>     'algo': 'selective-search',
+        >>> })
+        >>> exec(ut.execstr_dict(config), globals())
+        >>> downsample = False
+        >>> (gid, gpath, result_list) = detect_gid_list(ibs, gid_list, downsample, **config)
+        >>> result = ('(gid, gpath, result_list) = %s' % (ut.repr2((gid, gpath, result_list)),))
+        >>> print(result)
+        >>> kwargs = config = LocalizerConfig(**{
+        >>>     'algo': 'selective-search-rcnn',
+        >>> })
+        >>> exec(ut.execstr_dict(config), globals())
+        >>> downsample = False
+        >>> (gid, gpath, result_list) = detect_gid_list(ibs, gid_list, downsample, **config)
+        >>> result = ('(gid, gpath, result_list) = %s' % (ut.repr2((gid, gpath, result_list)),))
+        >>> print(result)
+        >>> ut.quit_if_noshow()
+        >>> import plottool as pt
+        >>> ut.show_if_requested()
+
+    Yields:
+        results (list of dict)
+    """
+    # Get new gpaths if downsampling
+    if downsample:
+        gpath_list = ibs.get_image_detectpaths(gid_list)
+        neww_list = [vt.open_image_size(gpath)[0] for gpath in gpath_list]
+        oldw_list = [oldw for (oldw, oldh) in ibs.get_image_sizes(gid_list)]
+        downsample_list = [oldw / neww for oldw, neww in zip(oldw_list, neww_list)]
+        orient_list = [1] * len(gid_list)
+    else:
+        gpath_list = ibs.get_image_paths(gid_list)
+        downsample_list = [None] * len(gpath_list)
+        orient_list = ibs.get_image_orientation(gid_list)
+    # Run detection
+    results_iter = detect(gpath_list, **kwargs)
+    # Upscale the results
+    _iter = zip(downsample_list, gid_list, orient_list, results_iter)
+    for downsample, gid, orient, (gpath, result_list) in _iter:
+        # Upscale the results back up to the original image size
+        for result in result_list:
+            ut.embed()
+            if downsample is not None and downsample != 1.0:
+                for key in ['xtl', 'ytl', 'width', 'height']:
+                    result[key] = int(result[key] * downsample)
+            bbox = (result['xtl'], result['ytl'], result['width'], result['height'], )
+            bbox_list = [ bbox ]
+            bbox_list = ibs.fix_horizontal_bounding_boxes_to_orient(gid, bbox_list)
+            bbox = bbox_list[0]
+            result['xtl'], result['ytl'], result['width'], result['height'] = bbox
+        yield (gid, gpath, result_list)
+
+
+def detect(gpath_list, matlab_command='selective_search', verbose=VERBOSE_SS, **kwargs):
+    """
+    Args:
+        gpath_list (list of str): the list of image paths that need proposal candidates
+
+    Kwargs (optional): refer to the Selective Search documentation for configuration settings
+
+    Returns:
+        iter
+    """
+    # Form the MATLAB script command that processes images and write to
+    # temporary results file.
+    temp_file, temp_filepath = tempfile.mkstemp(suffix='.mat')
+    os.close(temp_file)
+    if verbose:
+        gpath_str = '{%s}' % (','.join(map(str, gpath_list)))
+        matlab_command_str = '%s(%s, %r)' % (matlab_command, gpath_str, temp_filepath)
+        print('Calling: %s' % (matlab_command_str, ))
+
+    # Execute command in MATLAB.
+    bash_command = 'matlab -nojvm -r "try; %s; catch; exit; end; exit"'
+    bash_str = bash_command % (matlab_command_str, )
+    bash_list = shlex.split(bash_str)
+    with open('/dev/null', 'w') as null:
+        process_id = subprocess.Popen(bash_list, stdout=null, cwd=SCRIPT_PATH)
+        process_return_code = process_id.wait()
+        if process_return_code != 0:
+            raise Exception('Matlab script did not exit successfully!')
+
+    # Read the results and undo Matlab's 1-based indexing.
+    boxes_list = list(scipy.io.loadmat(temp_filepath)['all_boxes'][0])
+    subtractor = np.array((1, 1, 0, 0))[np.newaxis, :]
+    results_list = [boxes - subtractor for boxes in boxes_list]
+
+    # Remove temporary file, and return.
+    os.remove(temp_filepath)
+    if len(results_list) != len(gpath_list):
+        raise Exception('Something went wrong computing the windows!')
+    return results_list
