318afe40cb839e3d8cc6187b91b82824d5159303
Jason Parham <bluemellophone@gmail.com>
2017-02-03 12:51:31 -0500

    Fixes

--- ibeis/control/manual_image_funcs.py
+++ ibeis/control/manual_image_funcs.py
@@ -404,7 +404,6 @@ def localize_images(ibs, gid_list_=None):
     """
     #from os.path import isabs
     import six
-    import urllib
     import urlparse
     if gid_list_ is None:
         print('WARNING: you are localizing all gids')
@@ -439,14 +438,9 @@ def localize_images(ibs, gid_list_=None):
         if isproto(uri, valid_protos):
             # Ensure that the Unicode string is properly encoded for web requests
             uri_ = urlparse.urlsplit(uri)
-            uri_ = uri_.path.encode('utf8')
-            if six.PY2:
-                _quote = urllib.quote
-            else:
-                _quote = six.moves.urllib.parse.quote
-            uri_ = _quote(uri_)
-            uri_ = uri_._replace(path=uri_)
-            uri = uri_.geturl()
+            uri_path = six.moves.urllib.parse.quote(uri_.path.encode('utf8'))
+            uri_ = uri_._replace(path=uri_path)
+            uri2 = uri_.geturl()
         if isproto(uri, s3_proto):
             s3_dict = ut.s3_str_decode_to_dict(uri)
             ut.grab_s3_contents(loc_gpath, **s3_dict)
