1525b5834f252731a52d4771b4b05a8d0975c5af
Jason Parham <bluemellophone@gmail.com>
2017-03-09 22:49:14 -0500

    Better memory management for localizations_classifications

--- ibeis/core_images.py
+++ ibeis/core_images.py
@@ -934,7 +934,7 @@ class Classifier2Config(dtool.Config):
     coltypes=[np.ndarray, np.ndarray],
     configclass=Classifier2Config,
     fname='detectcache',
-    chunksize=16,
+    chunksize=8,
 )
 def compute_localizations_classifications(depc, loc_id_list, config=None):
     r"""
