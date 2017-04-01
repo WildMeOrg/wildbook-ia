aa544ac401acd28cf68fe653148384b3637dc784
joncrall <crallj@rpi.edu>
2017-04-01 15:21:32 -0400

    wip - Squashed 3 commits from Sat, 01 Apr 2017 19:20:17 -0400 to 19:21:32 -0400

--- ibeis/scripts/iccv.py
+++ ibeis/scripts/iccv.py
@@ -8,7 +8,8 @@ print, rrr, profile = ut.inject2(__name__)
 def gt_review():
     r"""
     CommandLine:
-        python -m ibeis.scripts.iccv gt_review
+        python -m ibeis.scripts.iccv gt_review --db PZ_MTEST
+        python -m ibeis.scripts.iccv gt_review --db GZ_Master1
 
     Example:
         >>> # DISABLE_DOCTEST
@@ -18,8 +19,9 @@ def gt_review():
     """
     import ibeis
     import ubelt as ub
-    # defaultdb = 'GZ_Master'
-    defaultdb = 'PZ_MTEST'
+
+    defaultdb = ut.get_argval('--db', default='GZ_Master1')
+    # defaultdb = 'PZ_MTEST'
     cacher = ub.Cacher('tmp_gz_review', defaultdb + 'v2')
     data = cacher.tryload()
     if data is None:
