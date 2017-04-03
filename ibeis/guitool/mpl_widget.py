55e14fb3bded3b28263da299cbabcb33f9c58b2f
joncrall <crallj@rpi.edu>
2017-04-03 18:54:43 -0400

    merged mpl widget from vtool and guitool

--- ibeis/guitool/mpl_widget.py
+++ ibeis/guitool/mpl_widget.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import utool as ut
 import guitool as gt
 import plottool as pt
+import matplotlib as mpl
 from guitool.__PYQT__.QtCore import Qt
 from guitool.__PYQT__ import QtCore, QtWidgets, QtGui  # NOQA
 from plottool import interact_helpers as ih
@@ -17,34 +18,61 @@ FigureCanvas = backend_qt.FigureCanvasQTAgg
 
 
 class MatplotlibWidget(gt.GuitoolWidget):
+    """
+    A qt widget that contains a matplotlib figure
+
+    References:
+        http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+    """
     click_inside_signal = QtCore.pyqtSignal(MouseEvent, object)
     key_press_signal = QtCore.pyqtSignal(KeyEvent)
     pick_event_signal = QtCore.pyqtSignal(PickEvent)
 
-    def initialize(self):
+    def initialize(self, pan_and_zoom=False):
         from plottool.interactions import zoom_factory, pan_factory
-        self.fig = pt.plt.figure()
+        from plottool import abstract_interaction
+
+        # Create unmanaged figure and a canvas
+        self.fig = mpl.figure.Figure()
         self.fig._no_raise_plottool = True
-        # Add a figure canvas widget to this widget
         self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+        self.addWidget(self.canvas)
+
         # Workaround key_press bug
         # References: https://github.com/matplotlib/matplotlib/issues/707
         self.canvas.setFocusPolicy(Qt.ClickFocus)
 
-        self.ax = self.fig.add_subplot(1, 1, 1)
-        self.addWidget(self.canvas)
+        self.reset_ax()
 
-        self.pan_events = pan_factory(self.ax)
-        self.zoon_events = zoom_factory(self.ax)
+        # self.ax = self.fig.add_subplot(1, 1, 1)
+        # pt.adjust_subplots(left=0, right=1, top=1, bottom=0, fig=self.fig)
+
+        if pan_and_zoom or True:
+            self.pan_events = pan_factory(self.ax)
+            self.zoon_events = zoom_factory(self.ax)
+
         self.fig.canvas.mpl_connect('button_press_event', self._emit_button_press)
         self.fig.canvas.mpl_connect('key_press_event', self.key_press_signal.emit)
         self.fig.canvas.mpl_connect('pick_event', self.pick_event_signal.emit)
 
-        # self.MOUSE_BUTTONS = abstract_interaction.AbstractInteraction.MOUSE_BUTTONS
+        self.MOUSE_BUTTONS = abstract_interaction.AbstractInteraction.MOUSE_BUTTONS
         self.setMinimumHeight(20)
         self.setMinimumWidth(20)
 
         self.installEventFilter(self.parent())
+
+    def clf(self):
+        self.fig.clf()
+        self.reset_ax()
+
+    def reset_ax(self):
+        # from plottool.interactions import zoom_factory, pan_factory
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        pt.adjust_subplots(left=0, right=1, top=1, bottom=0, fig=self.fig)
+        # self.pan_events = pan_factory(self.ax)
+        # self.zoon_events = zoom_factory(self.ax)
+        return self.ax
 
     def _emit_button_press(self, event):
         if ih.clicked_inside_axis(event):
