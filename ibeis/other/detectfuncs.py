1463807db999153fc4d31510e644c8f3069f4ac9
Jason Parham <bluemellophone@gmail.com>
2017-07-25 14:13:25 -0400

    TRAIN_SET

--- ibeis/other/detectfuncs.py
+++ ibeis/other/detectfuncs.py
@@ -2238,7 +2238,7 @@ def classifier_binary_precision_recall_algo_display(ibs, figsize=(16, 16), **kwa
 
 def classifier2_precision_recall_algo(ibs, category, **kwargs):
     depc = ibs.depc_image
-    test_gid_set = set(general_get_imageset_gids(ibs, 'TEST_SET'))
+    test_gid_set = set(general_get_imageset_gids(ibs, 'TRAIN_SET'))
     test_gid_set = list(test_gid_set)
     aids_list = ibs.get_image_aids(test_gid_set)
     species_set_list = [
@@ -2357,7 +2357,7 @@ def classifier2_precision_recall_algo_display(ibs, species_list=None,
     # kwargs['classifier_two_weight_filepath'] = 'v3'
     kwargs['classifier_two_weight_filepath'] = 'candidacy'
 
-    test_gid_set = set(general_get_imageset_gids(ibs, 'TEST_SET'))
+    test_gid_set = set(general_get_imageset_gids(ibs, 'TRAIN_SET'))
     test_gid_set = list(test_gid_set)
     depc = ibs.depc_image
 
