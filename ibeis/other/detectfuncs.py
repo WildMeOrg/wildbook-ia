7b219d16b1142bfbe1be067efd4e86b6282e2c4e
Jason Parham <bluemellophone@gmail.com>
2017-08-29 21:39:21 -0400

    Train AOI

--- ibeis/other/detectfuncs.py
+++ ibeis/other/detectfuncs.py
@@ -4852,6 +4852,24 @@ def background_train(ibs, species):
     return model_path
 
 
+@register_ibs_method
+def aoi_train(ibs, species_list=None):
+    from ibeis_cnn.ingest_ibeis import get_aoi_training_data
+    from ibeis_cnn.process import numpy_processed_directory4
+    from ibeis_cnn.models.aoi import train_aoi
+    from ibeis_cnn.utils import save_model
+    data_path = join(ibs.get_cachedir(), 'extracted')
+    extracted_path = get_aoi_training_data(ibs, data_path, target_species_list=species_list)
+    id_file, X_file, y_file = numpy_processed_directory4(extracted_path)
+    output_path = join(ibs.get_cachedir(), 'training', 'background')
+    model_path = train_aoi(output_path, X_file, y_file)
+    model_state = ut.load_cPkl(model_path)
+    assert 'species_list' not in model_state
+    model_state['species_list'] = species_list
+    save_model(model_state, model_path)
+    return model_path
+
+
 def _resize(image, t_width=None, t_height=None, verbose=False):
     if verbose:
         print('RESIZING WITH t_width = %r and t_height = %r' % (t_width, t_height, ))
