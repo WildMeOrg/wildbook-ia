3fd7cbd0ca5a8ca25c2fa9d8a1266c8c6f15190d
joncrall <crallj@rpi.edu>
2017-11-01 15:39:56 -0400

    wip

--- ibeis/algo/verif/torch/lr_schedule.py
+++ ibeis/algo/verif/torch/lr_schedule.py
@@ -1,16 +1,23 @@
 
 class Exponential(object):
+    """
+    Example:
+        >>> from ibeis.algo.verif.torch.lr_schedule import *
+        >>> lr_scheduler = Exponential()
+        >>> lr_scheduler(0)
+    """
     def __init__(self, init_lr=0.001, lr_decay_epoch=2):
         self.init_lr = init_lr
         self.lr_decay_epoch = lr_decay_epoch
 
-    def __call__(self, epoch, optimizer):
+    def __call__(self, epoch, optimizer=None):
         """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
         lr = self.init_lr
-        if epoch % self.lr_decay_epoch == 0 and epoch is not 0:
+
+        if epoch > 0 and epoch % self.lr_decay_epoch == 0:
             lr *= 0.1
-        if epoch % self.lr_decay_epoch == 0:
-            print('LR is set to {}'.format(lr))
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+
+        if optimizer is not None:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
         return lr
