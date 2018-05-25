d307ee92cde065aa2975503e92d2a1eebc6f6167
Jason Parham <parham@wildme.org>
2018-05-25 12:53:50 -0700

    Updates

--- ibeis/control/manual_annot_funcs.py
+++ ibeis/control/manual_annot_funcs.py
@@ -686,7 +686,7 @@ def annotation_src_api(rowid=None):
 def filter_annotation_set(ibs, aid_list, include_only_gid_list=None,
                           yaw='no-filter', is_exemplar=None, is_staged=False,
                           species=None, is_known=None, hasgt=None, minqual=None,
-                          has_timestamp=None, sort=True, min_timedelta=None):
+                          has_timestamp=None, sort=False, min_timedelta=None):
     # -- valid aid filtering --
     # filter by is_exemplar
     if is_exemplar is True:
