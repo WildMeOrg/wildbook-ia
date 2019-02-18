144f289a051173bf6fe8471becfca8e1c72d1f2a
Jason Parham <parham@wildme.org>
2019-02-18 15:54:03 -0800

    Updats

--- ibeis/algo/detect/canonical.py
+++ ibeis/algo/detect/canonical.py
@@ -156,7 +156,7 @@ class ImageFilePathList(torch.utils.data.Dataset):
         return fmt_str
 
 
-def finetune(model, dataloaders, optimizer, scheduler, device, num_epochs=256, under=1.0, over=2.0):
+def finetune(model, dataloaders, optimizer, scheduler, device, num_epochs=128, under=1.0, over=2.0):
     phases = ['train', 'val']
 
     start = time.time()
@@ -339,7 +339,7 @@ def visualize_augmentations(dataset, augmentation, tag, num=20):
     plt.imsave(canvas_filepath, canvas)
 
 
-def train(data_path, output_path, batch_size=8):
+def train(data_path, output_path, batch_size=32):
     # Detect if we have a GPU available
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
