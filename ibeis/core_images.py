397fd7bb5c7dd555175b4b306125e140bd5e1b31
Jason Parham <parham@wildme.org>
2019-06-14 10:39:09 -0700

    Allow for more memory usage on testing

--- ibeis/core_images.py
+++ ibeis/core_images.py
@@ -251,7 +251,7 @@ class ClassifierConfig(dtool.Config):
     coltypes=[float, str],
     configclass=ClassifierConfig,
     fname='detectcache',
-    chunksize=64 if const.PRODUCTION else 14336,
+    chunksize=256 if const.PRODUCTION else 14336,
 )
 def compute_classifications(depc, gid_list, config=None):
     r"""Extract the detections for a given input image.
@@ -769,7 +769,7 @@ class LocalizerOriginalConfig(dtool.Config):
     coltypes=[float, np.ndarray, np.ndarray, np.ndarray, np.ndarray],
     configclass=LocalizerOriginalConfig,
     fname='localizationscache',
-    chunksize=32 if const.PRODUCTION else 1536,
+    chunksize=64 if const.PRODUCTION else 1536,
 )
 def compute_localizations_original(depc, gid_list, config=None):
     r"""Extract the localizations for a given input image.
@@ -1263,7 +1263,7 @@ class LocalizerConfig(dtool.Config):
     coltypes=[float, np.ndarray, np.ndarray, np.ndarray, np.ndarray],
     configclass=LocalizerConfig,
     fname='detectcache',
-    chunksize=128 if const.PRODUCTION else 8192,
+    chunksize=256 if const.PRODUCTION else 8192,
 )
 def compute_localizations(depc, loc_orig_id_list, config=None):
     r"""Extract the localizations for a given input image.
