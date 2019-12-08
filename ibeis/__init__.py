907204293e980ff94e773f9ab8ad172b9c1ff724
joncrall <erotemic@gmail.com>
2019-12-07 23:08:06 -0500

    Fix requirements

--- ibeis/__init__.py
+++ ibeis/__init__.py
@@ -8,7 +8,7 @@ TODO: LAZY IMPORTS?
 # flake8: noqa
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 try:
     import utool as ut
