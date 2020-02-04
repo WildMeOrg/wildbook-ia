02bb3a451e4af133f137f88df5a32a26b6613ba9
Jason Parham <parham@wildme.org>
2020-02-04 14:32:01 -0800

    Added refer for skunks

--- ibeis/web/routes.py
+++ ibeis/web/routes.py
@@ -4113,7 +4113,42 @@ def turk_identification_graph_refer(imgsetid, species=None, tier=1, year=2019, o
         annot_uuid_list = ibs.get_annot_uuids(aid_list_)
         return turk_identification_graph(annot_uuid_list=annot_uuid_list, hogwild_species=species,
                                          creation_imageset_rowid_list=[imgsetid])
-    elif option in ['rosemary']:
+    elif ibs.dbname == 'SSKUNK_Master' or option in ['skunk']:
+        species = 'skunk_spotted'
+
+        gid_list = ibs.get_imageset_gids(imgsetid)
+        image_reviewed_list = ibs.get_image_reviewed(gid_list)
+        gid_list = ut.compress(gid_list, image_reviewed_list)
+
+        aid_list = ut.flatten(ibs.get_image_aids(gid_list))
+        species_list = ibs.get_annot_species(aid_list)
+        viewpoint_list = ibs.get_annot_viewpoints(aid_list)
+        quality_list = ibs.get_annot_quality_texts(aid_list)
+
+        bad_flag_list = [species_ == const.UNKNOWN for species_ in species_list]
+        bad_aid_list = ut.compress(aid_list, bad_flag_list)
+        if len(bad_aid_list) > 0:
+            ibs.set_annot_species(bad_aid_list, [species] * len(bad_aid_list))
+            species_list = ibs.get_annot_species(aid_list)
+
+        flag_list = []
+        for aid, species_, viewpoint, quality in zip(aid_list, species_list, viewpoint_list, quality_list):
+            flag = True
+            if species_ != species:
+                flag = False
+            if viewpoint is None or 'up' not in viewpoint:
+                flag = False
+            if quality in ['junk', 'poor']:
+                flag = False
+            flag_list.append(flag)
+
+        aid_list_ = ut.compress(aid_list, flag_list)
+
+        imageset_text = ibs.get_imageset_text(imgsetid).lower()
+        annot_uuid_list = ibs.get_annot_uuids(aid_list_)
+        return turk_identification_graph(annot_uuid_list=annot_uuid_list, hogwild_species=species,
+                                         creation_imageset_rowid_list=[imgsetid])
+     elif option in ['rosemary']:
         imgsetid_ = ibs.get_imageset_imgsetids_from_text('RosemaryLoopsData')
 
         assert imgsetid == imgsetid_
