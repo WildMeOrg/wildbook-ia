558fc55dc41bde9b2092f786dde2f5eddf0243f9
karen chan <karen@karen-chan.com>
2020-05-20 14:00:51 +0100

    Copy Dockerfiles from private dev repo to devops/
    `devops/Dockerfile`, `devops/base/Dockerfile` and
    `devops/dependencies/Dockerfile` are unchanged and
    `devops/provision/Dockerfile` has been modified to use public repos on
    github instead of private azure ones.
    
    `devops/provision/_config/types_c.h` is required by the provision
    Dockerfile so has also been copied.
    
    `theanorc`, `embed.sh`, `healthcheck.py` and `boto.cfg` in
    `devops/provision/_config/` are required by the ibeis Dockerfile so have
    been copied.  The aws access key id and secret access key have been
    removed from `boto.cfg`.

---
+++ devops/provision/_config/healthcheck.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+config = {
+    'protocol' : 'http',
+    'server'   : 'localhost',
+    'port'     :  5000,
+    'endpoint' : '/api/test/heartbeat/',
+}
+
+args = (config['protocol'], config['server'], config['port'], config['endpoint'], )
+url = '%s://%s:%s%s' % args
+
+try:
+    raw = requests.get(url, timeout=120)
+    response = raw.json()
+    healthy = response.get('response')
+
+    if not healthy:
+        raise RuntimeError
+
+    sys.exit(0)
+except Exception as ex:
+    sys.exit(1)
