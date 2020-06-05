94f3f3151b0065d2df5a8e2922129c91524b9152
Jason Parham <bluemellophone@gmail.com>
2020-06-05 09:50:59 -0700

    Added support for cleaning IBEIS properly

--- clean.sh
+++ clean.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#!/bin/bash
+
+rm -rf __pycache__
 rm -rf _skbuild
 rm -rf dist
 rm -rf build
 rm -rf wbia.egg-info
 
+rm -rf ibeis
+rm -rf ibeis.egg-info
+
 rm -rf mb_work
 rm -rf wheelhouse
 
-CLEAN_PYTHON='find . -regex ".*\(__pycache__\|\.py[co]\)" -delete || find . -iname *.pyc -delete || find . -iname *.pyo -delete'
+CLEAN_PYTHON='find . -iname __pycache__ -delete && find . -iname *.pyc -delete && find . -iname *.pyo -delete'
 bash -c "$CLEAN_PYTHON"
