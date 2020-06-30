edadfcb8dabb23a3feafe1815ebbae0183503e30
karen chan <karen@karen-chan.com>
2020-06-30 21:05:38 +0100

    Delete dev/docker, replaced with devops/
    The `devops/` docker files can be used for development instead.

--- dev/docker/build.sh
+++
@@ -1,7 +0,0 @@
-git clone git@github.com:WildbookOrg/dev.git
-docker build --target com.wildme.wildbook-image-curation.test --tag wildme/wildbook-image-curation-test .
-docker build --target com.wildme.wildbook-image-curation.deploy --tag wildme/wildbook-image-curation .
-# sudo port install socat
-# socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-# docker run -e DISPLAY=docker.for.mac.host.internal:0 wildme/wildbook-image-curation-test
-# docker run -p 5000:5000 wildme/wildbook-image-curation
