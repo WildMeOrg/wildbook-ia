ae642cdc99f475041e7815d46005f22bf8e16af9
Michael Mulich <michael.mulich@gmail.com>
2020-07-10 11:07:13 -0700

    Unroot the container build script

--- devops/build.sh
+++ devops/build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-cd base && docker build -t wildme/wbia-base:latest .
-cd ..
-cd dependencies && docker build -t wildme/wbia-dependencies:latest .
-cd ..
-cd provision && docker build -t wildme/wbia-provision:latest .
-cd ..
+set -ex
+
+# See https://stackoverflow.com/a/246128/176882
+export ROOT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Change to the script's root directory location
+cd ${ROOT_LOC}
+
+# Build the images in dependence order
+docker build -t wildme/wbia-base:latest base
+docker build -t wildme/wbia-dependencies:latest dependencies
+docker build -t wildme/wbia-provision:latest provision
 docker build -t wildme/wbia:latest .
