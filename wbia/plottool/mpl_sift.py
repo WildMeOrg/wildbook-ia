dc42a3fdfe65f8ab881e13be60bab3807bcd680b
karen chan <karen@karen-chan.com>
2020-07-23 13:05:26 +0100

    Create Affine2D objects only if necessary in plottool/mpl_sift.py
    When running `pytest wbia/plottool/mpl_keypoint.py::draw_keypoints:0`:
    
    ```
    DOCTEST TRACEBACK
    Traceback (most recent call last):
      File "/virtualenv/env3/lib/python3.6/site-packages/xdoctest/doctest_example.py", line 556, in run
        exec(code, test_globals)
      File "<doctest:/wbia/wildbook-ia/wbia/plottool/mpl_keypoint.py::draw_keypoints:0>", line rel: 31, abs: 107, in <module>
        ...                rect, eig, ori, sifts, siftkw, H=H, **kwargs)
      File "/wbia/wildbook-ia/wbia/plottool/mpl_keypoint.py", line 163, in draw_keypoints
        mpl_sift.draw_sifts(ax, sifts, invVR_aff2Ds, **siftkw)
      File "/wbia/wildbook-ia/wbia/plottool/mpl_sift.py", line 280, in draw_sifts
        invVR_aff2Ds = [mpl.transforms.Affine2D(matrix=aff_) for aff_ in invVR_aff2Ds]
      File "/wbia/wildbook-ia/wbia/plottool/mpl_sift.py", line 280, in <listcomp>
        invVR_aff2Ds = [mpl.transforms.Affine2D(matrix=aff_) for aff_ in invVR_aff2Ds]
      File "/virtualenv/env3/lib/python3.6/site-packages/matplotlib/transforms.py", line 1846, in __init__
        self._mtx = matrix.copy()
    AttributeError: 'Affine2D' object has no attribute 'copy'
    ```
    
    It seems that `aff_` is already a `Affine2D` object, and that's causing the
    failure, so check before creating `Affine2D` objects.

--- wbia/plottool/mpl_sift.py
+++ wbia/plottool/mpl_sift.py
@@ -277,7 +277,12 @@ def draw_sifts(ax, sifts, invVR_aff2Ds=None, **kwargs):
     if invVR_aff2Ds is None:
         invVR_aff2Ds = [mpl.transforms.Affine2D() for _ in range(len(sifts))]
     if isinstance(invVR_aff2Ds, (list, np.ndarray)):
-        invVR_aff2Ds = [mpl.transforms.Affine2D(matrix=aff_) for aff_ in invVR_aff2Ds]
+        invVR_aff2Ds = [
+            aff_
+            if isinstance(aff_, mpl.transforms.Affine2D)
+            else mpl.transforms.Affine2D(matrix=aff_)
+            for aff_ in invVR_aff2Ds
+        ]
     colltup_list = [
         get_sift_collection(sift, aff, **kwargs) for sift, aff in zip(sifts, invVR_aff2Ds)
     ]
