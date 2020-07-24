5aa7eb77b6c310502f9d1b251e5738627d7233e0
Michael Mulich <michael.mulich@gmail.com>
2020-07-24 11:52:19 -0700

    Remove testing pathways from wbia.__main__
    Standardizing on pytest execution of tests. This removes a parallel
    and unsupported invocation of tests.

--- wbia/__main__.py
+++ wbia/__main__.py
@@ -67,16 +67,7 @@ def run_wbia():
     if ub.argflag('--devcmd'):
         # Hack to let devs mess around when using an installer version
         # TODO: add more hacks
-        # import utool.tests.run_tests
-        # utool.tests.run_tests.run_tests()
         ut.embed()
-    # Run the tests of other modules
-    elif ub.argflag('--run-utool-tests'):
-        raise Exception('Deprecated functionality')
-    elif ub.argflag('--run-vtool-tests'):
-        raise Exception('Deprecated functionality')
-    elif ub.argflag(('--run-wbia-tests', '--run-tests')):
-        raise Exception('Deprecated functionality')
 
     if ub.argflag('-e'):
         """
@@ -89,57 +80,12 @@ def run_wbia():
         print('... exiting')
         sys.exit(0)
 
-    # Attempt to run a test using the funciton name alone
-    # with the --tf flag
-    # if False:
-    #     import wbia.tests.run_tests
-    #     import wbia.tests.reset_testdbs
-    #     import wbia.scripts.thesis
-    #     ignore_prefix = [
-    #         #'wbia.tests',
-    #         'wbia.control.__SQLITE3__',
-    #         '_autogen_explicit_controller']
-    #     ignore_suffix = ['_grave']
-    #     func_to_module_dict = {
-    #         'demo_bayesnet': 'wbia.unstable.demobayes',
-    #     }
-    #     ut.main_function_tester('wbia', ignore_prefix, ignore_suffix,
-    #                             func_to_module_dict=func_to_module_dict)
-
     # if ub.argflag('-e'):
     #    import wbia
     #    expt_kw = ut.get_arg_dict(ut.get_func_kwargs(wbia.run_experiment),
     #    prefix_list=['--', '-'])
     #    wbia.run_experiment(**expt_kw)
     #    sys.exit(0)
-
-    doctest_modname = ut.get_argval(
-        ('--doctest-module', '--tmod', '-tm', '--testmod'),
-        type_=str,
-        default=None,
-        help_='specify a module to doctest',
-    )
-    if doctest_modname is not None:
-        """
-        Allow any doctest to be run the main wbia script
-
-        python -m wbia --tmod utool.util_str --test-align:0
-        python -m wbia --tmod wbia.algo.hots.pipeline --test-request_wbia_query_L0:0 --show
-        python -m wbia --tf request_wbia_query_L0:0 --show
-        ./dist/wbia/IBEISApp --tmod wbia.algo.hots.pipeline --test-request_wbia_query_L0:0 --show  # NOQA
-        ./dist/wbia/IBEISApp --tmod utool.util_str --test-align:0
-        ./dist/IBEIS.app/Contents/MacOS/IBEISApp --tmod utool.util_str --test-align:0
-        ./dist/IBEIS.app/Contents/MacOS/IBEISApp --run-utool-tests
-        ./dist/IBEIS.app/Contents/MacOS/IBEISApp --run-vtool-tests
-        """
-        print('[wbia] Testing module')
-        mod_alias_list = {'exptdraw': 'wbia.expt.experiment_drawing'}
-        doctest_modname = mod_alias_list.get(doctest_modname, doctest_modname)
-        module = ut.import_modname(doctest_modname)
-        (nPass, nTotal, failed_list, error_report_list) = ut.doctest_funcs(module=module)
-        retcode = 1 - (len(failed_list) == 0)
-        # print(module)
-        sys.exit(retcode)
 
     import wbia
 
