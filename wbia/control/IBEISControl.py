8e6ab17212d3351fd3e8d9ecdfee865e6f44b685
karen chan <karen@karen-chan.com>
2020-07-27 19:29:50 +0100

    Rename ibeis_flukematch references to wbia_flukematch
    We renamed the plugin to wbia_flukematch recently.

--- wbia/control/IBEISControl.py
+++ wbia/control/IBEISControl.py
@@ -104,7 +104,7 @@ AUTOLOAD_PLUGIN_MODNAMES = [
     'wbia.algo.smk.smk_pipeline',
     # (('--no-cnn', '--nocnn'), 'wbia_cnn'),
     (('--no-cnn', '--nocnn'), 'wbia_cnn._plugin'),
-    # (('--no-fluke', '--nofluke'), 'ibeis_flukematch.plugin'),
+    # (('--no-fluke', '--nofluke'), 'wbia_flukematch.plugin'),
     # (('--no-curvrank', '--nocurvrank'), 'wbia_curvrank._plugin'),
     # 'wbia_plugin_identification_example',
 ]
