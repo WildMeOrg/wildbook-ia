345ee69fcb019c48778649daa65e661aa4fa7c84
Michael Mulich <michael.mulich@gmail.com>
2020-08-06 23:35:39 -0700

    Add the runtime container to the container build scripts
    Build and publish the `wildme/wildbook-ia` image with the other images

--- devops/build.sh
+++ devops/build.sh
@@ -13,3 +13,7 @@ docker build -t wildme/wbia-base:latest base
 docker build -t wildme/wbia-dependencies:latest dependencies
 docker build -t wildme/wbia-provision:latest provision
 docker build -t wildme/wbia:latest .
+
+cd ../
+# Build the runtime container
+docker build -t wildbook/wildbook-ia:latest .
