5ea9d73cf920631b49920726257b8d8ef7bad535
Michael Mulich <michael.mulich@gmail.com>
2020-08-11 22:37:05 -0700

    Lift initialization of testdb0
    Lift the function that specifically initializes testdb0 to a place
    higher where it can be used individually.

--- wbia/tests/reset_testdbs.py
+++ wbia/tests/reset_testdbs.py
@@ -37,69 +37,69 @@ def delete_dbdir(dbname):
     ut.delete(join(wbia.sysres.get_workdir(), dbname), ignore_errors=False)
 
 
+def make_testdb0():
+    """ makes testdb0 """
+
+    def get_test_gpaths(ndata=None, names=None, **kwargs):
+        # Read ndata from args or command line
+        """ DEPRICATE """
+        ndata_arg = ut.get_argval(
+            '--ndata',
+            type_=int,
+            default=None,
+            help_='use --ndata to specify bigger data',
+        )
+        if ndata_arg is not None:
+            ndata = ndata_arg
+        imgdir = get_testdata_dir(**kwargs)
+        gpath_list = sorted(list(ut.list_images(imgdir, full=True, recursive=True)))
+        # Get only the gpaths of certain names
+        if names is not None:
+            gpath_list = [
+                gpath for gpath in gpath_list if ut.basename_noext(gpath) in names
+            ]
+        # Get a some number of test images
+        if ndata is not None:
+            gpath_cycle = cycle(gpath_list)
+            if six.PY2:
+                gpath_list = [gpath_cycle.next() for _ in range(ndata)]
+            else:
+                gpath_list = [next(gpath_cycle) for _ in range(ndata)]
+        return gpath_list
+
+    workdir = wbia.sysres.get_workdir()
+    TESTDB0 = join(workdir, 'testdb0')
+    main_locals = wbia.main(dbdir=TESTDB0, gui=False, allow_newdir=True)
+    ibs = main_locals['ibs']
+    assert ibs is not None, str(main_locals)
+    gpath_list = list(map(ut.unixpath, get_test_gpaths()))
+    # print('[RESET] gpath_list=%r' % gpath_list)
+    gid_list = ibs.add_images(gpath_list)  # NOQA
+    valid_gids = ibs.get_valid_gids()
+    valid_aids = ibs.get_valid_aids()
+    try:
+        assert (
+            len(valid_aids) == 0
+        ), 'there are more than 0 annotations in an empty database!'
+    except Exception as ex:
+        ut.printex(ex, key_list=['valid_aids'])
+        raise
+    gid_list = valid_gids[0:1]
+    bbox_list = [(0, 0, 100, 100)]
+    aid = ibs.add_annots(gid_list, bbox_list=bbox_list)[0]
+    # print('[RESET] NEW RID=%r' % aid)
+    aids = ibs.get_image_aids(gid_list)[0]
+    try:
+        assert aid in aids, 'bad annotation adder: aid = %r, aids = %r' % (aid, aids,)
+    except Exception as ex:
+        ut.printex(ex, key_list=['aid', 'aids'])
+        raise
+
+
 def ensure_smaller_testingdbs():
     """
     Makes the smaller test databases
     """
-
-    def make_testdb0():
-        """ makes testdb0 """
-
-        def get_test_gpaths(ndata=None, names=None, **kwargs):
-            # Read ndata from args or command line
-            """ DEPRICATE """
-            ndata_arg = ut.get_argval(
-                '--ndata',
-                type_=int,
-                default=None,
-                help_='use --ndata to specify bigger data',
-            )
-            if ndata_arg is not None:
-                ndata = ndata_arg
-            imgdir = get_testdata_dir(**kwargs)
-            gpath_list = sorted(list(ut.list_images(imgdir, full=True, recursive=True)))
-            # Get only the gpaths of certain names
-            if names is not None:
-                gpath_list = [
-                    gpath for gpath in gpath_list if ut.basename_noext(gpath) in names
-                ]
-            # Get a some number of test images
-            if ndata is not None:
-                gpath_cycle = cycle(gpath_list)
-                if six.PY2:
-                    gpath_list = [gpath_cycle.next() for _ in range(ndata)]
-                else:
-                    gpath_list = [next(gpath_cycle) for _ in range(ndata)]
-            return gpath_list
-
-        workdir = wbia.sysres.get_workdir()
-        TESTDB0 = join(workdir, 'testdb0')
-        main_locals = wbia.main(dbdir=TESTDB0, gui=False, allow_newdir=True)
-        ibs = main_locals['ibs']
-        assert ibs is not None, str(main_locals)
-        gpath_list = list(map(ut.unixpath, get_test_gpaths()))
-        # print('[RESET] gpath_list=%r' % gpath_list)
-        gid_list = ibs.add_images(gpath_list)  # NOQA
-        valid_gids = ibs.get_valid_gids()
-        valid_aids = ibs.get_valid_aids()
-        try:
-            assert (
-                len(valid_aids) == 0
-            ), 'there are more than 0 annotations in an empty database!'
-        except Exception as ex:
-            ut.printex(ex, key_list=['valid_aids'])
-            raise
-        gid_list = valid_gids[0:1]
-        bbox_list = [(0, 0, 100, 100)]
-        aid = ibs.add_annots(gid_list, bbox_list=bbox_list)[0]
-        # print('[RESET] NEW RID=%r' % aid)
-        aids = ibs.get_image_aids(gid_list)[0]
-        try:
-            assert aid in aids, 'bad annotation adder: aid = %r, aids = %r' % (aid, aids,)
-        except Exception as ex:
-            ut.printex(ex, key_list=['aid', 'aids'])
-            raise
-
     get_testdata_dir(True)
     if not ut.checkpath(join(wbia.sysres.get_workdir(), 'testdb0'), verbose=True):
         print('\n\nMAKE TESTDB0\n\n')
