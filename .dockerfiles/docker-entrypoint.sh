903a6b4b5df47d6ac8343cb47a5a0aab5c443efb
Michael Mulich <michael.mulich@gmail.com>
2020-08-28 21:12:41 -0700

    Install development code during container runtime
    If the `/code` directory is mounted within the container, the
    entrypoint script will uninstall the currently installed version of
    wildbook-ia in favor of the code in `/code`. This turns out runtime
    environment into a development environment.

--- .dockerfiles/docker-entrypoint.sh
+++ .dockerfiles/docker-entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# Use the user mounted wildbook-ia code to if mounted at /code
+if [ -d /code ]; then
+    echo "*** $0 --- Uninstalling wildbook-ia"
+    pip uninstall -y wildbook-ia
+    echo "*** $0 --- Installing development version of wildbook-ia at /code"
+    pushd /code && pip install -e ".[tests]" && popd
+fi
+
+
+echo "*** $0 --- progressing to main execution"
+
 # Catch all for CMD or 'command' in docker-compose
 # Executes as `wbia` with PID 1
 exec gosu wbia:wbia $@
