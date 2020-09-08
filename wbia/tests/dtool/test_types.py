8265134e142ed4859001e5b91806156f574ac2d8
Michael Mulich <michael.mulich@gmail.com>
2020-09-07 23:52:26 -0700

    Verify SA coercion of numpy floats to Float column type

--- wbia/tests/dtool/test_types.py
+++ wbia/tests/dtool/test_types.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+import numpy as np
 import pytest
 from sqlalchemy.engine import create_engine
 from sqlalchemy.sql import text, bindparam
+from sqlalchemy.types import Float
 
 from wbia.dtool.types import Dict, List, UUID
 
@@ -55,6 +57,29 @@ def test_list(db):
     stmt = text('select x from test')
     # Hint: https://docs.sqlalchemy.org/en/13/core/tutorial.html#specifying-result-column-behaviors
     stmt = stmt.columns(x=List)
+    results = db.execute(stmt)
+    selected_value = results.fetchone()[0]
+    assert selected_value == insert_value
+
+
+@pytest.mark.parametrize('num_type', (np.float32, np.float64))
+def test_numpy_floats(db, num_type):
+    # Verifies that a numpy float can be translated
+
+    # Create a table that uses the type
+    db.execute(text('CREATE TABLE test(x REAL)'))
+
+    # Insert a uuid value into the table
+    insert_value = num_type(8.0000008)
+    # Hint: https://docs.sqlalchemy.org/en/13/core/tutorial.html#specifying-bound-parameter-behaviors
+    stmt = text('INSERT INTO test(x) VALUES (:x)')
+    stmt = stmt.bindparams(bindparam('x', type_=Float))
+    db.execute(stmt, x=insert_value)
+
+    # Query for the value
+    stmt = text('SELECT x FROM test')
+    # Hint: https://docs.sqlalchemy.org/en/13/core/tutorial.html#specifying-result-column-behaviors
+    stmt = stmt.columns(x=Float)
     results = db.execute(stmt)
     selected_value = results.fetchone()[0]
     assert selected_value == insert_value
