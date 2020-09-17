19d15195a5b662f66231db3ffd3df02b7a10ddb6
Jason Parham <bluemellophone@gmail.com>
2020-09-16 17:05:24 -0700

    Bump version of documentation

--- _docs/conf.py
+++ _docs/conf.py
@@ -31,10 +31,10 @@ copyright = '2020, Wild Me'
 author = 'Jon Crall, Jason Parham, WildMe Developers'
 
 # The short X.Y version
-version = '3.0.2'
+version = '3.3.0'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.2'
+release = '3.3.0'
 
 
 # -- General configuration ---------------------------------------------------
