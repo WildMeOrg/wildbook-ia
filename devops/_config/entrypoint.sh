f1f9e04a9bcac79ee97e90a597ddd503f72b305b
Jason Parham <bluemellophone@gmail.com>
2020-09-18 13:48:30 -0700

    Support the case when no /data/db folder is provided

--- devops/_config/entrypoint.sh
+++ devops/_config/entrypoint.sh
@@ -6,4 +6,11 @@ addgroup --system ${HOST_USER}
 
 adduser --uid ${HOST_UID} --system --group ${HOST_USER}
 
+if [ ! -d "/data/db" ]
+then
+   mkdir -p /data/db
+   chown ${HOST_USER}:${HOST_USER} /data/db
+   chmod 750 /data/db
+fi
+
 exec gosu ${HOST_USER}:${HOST_USER} $@
