cc6ab15bca9d3634e1328b21b8533a024611c9aa
Michael Mulich <michael.mulich@gmail.com>
2020-11-11 00:31:28 -0800

    Add a script for building the documentation

---
+++ scripts/build-docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Use the bash script location (at $0) to determine the project root.
+PROJECT_ROOT=$(python3 -c "from pathlib import Path; print(Path('$0').parent.parent.resolve())")
+
+cd $PROJECT_ROOT
+pip install -r requirements/docs.txt
+cd docs
+make html
