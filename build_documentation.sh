8353d6bdfe17af14d9c705ebe1dc33eeb0c84e60
Michael Mulich <michael.mulich@gmail.com>
2020-11-11 00:31:28 -0800

    Add a script for building the documentation

--- build_documentation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# python -m utool.util_setup --exec-autogen_sphinx_apidoc  --nomake
-
-make -C _docs html
-rm -rf docs/
-mkdir -p docs/
-cp -r _docs/_build/html/* docs/
-touch docs/.nojekyll
