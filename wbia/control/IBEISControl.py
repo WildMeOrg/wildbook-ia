0b097bd993446b921bb548b9aefa59965ff88895
Jason Parham <bluemellophone@gmail.com>
2020-11-26 01:20:59 -0800

    Add repository for the orientation plugin

--- wbia/control/IBEISControl.py
+++ wbia/control/IBEISControl.py
@@ -140,6 +140,12 @@ if ut.get_argflag('--orient2d'):
         (('--no-2d-orient', '--no2dorient'), 'wbia_2d_orientation._plugin'),
     ]
 
+
+if ut.get_argflag('--orient'):
+    AUTOLOAD_PLUGIN_MODNAMES += [
+        (('--no-orient', '--noorient'), 'wbia_orientation._plugin'),
+    ]
+
 if ut.get_argflag('--pie'):
     AUTOLOAD_PLUGIN_MODNAMES += [
         (('--no-pie', '--nopie'), 'wbia_pie._plugin'),
