5edd071c2ababc2f43591c7f656d98bee67df2c0
Jason Parham <bluemellophone@gmail.com>
2021-01-11 14:29:51 -0800

    Small linting fixes

--- wbia/core_annots.py
+++ wbia/core_annots.py
@@ -2711,7 +2711,9 @@ def get_annot_lrudfb_bools(ibs, aid_list):
             'front' in view,
             'back' in view,
         ]
-        if view is not None else [False] * 6 for view in views
+        if view is not None
+        else [False] * 6
+        for view in views
     ]
     return bool_arrays
 
