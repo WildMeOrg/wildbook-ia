9f40ff374afbeee9d0936f4ce7e3a380e99eb739
karen chan <karen@karen-chan.com>
2021-01-22 00:13:51 +0000

    Add partial match for compare databases script
    Use `--db-dir` and `pg-uri` to check all sqlite databases in a db dir
    have all been migrated to postgresql.  It's also possible to compare a
    particular sqlite database has been migrated by using `--sqlite-uri`.

--- wbia/cli/compare_databases.py
+++ wbia/cli/compare_databases.py
@@ -13,22 +13,30 @@ from wbia.dtool.copy_sqlite_to_postgres import (
 @click.option(
     '--db-dir',
     multiple=True,
-    help='SQLite database(s) location, can be a directory or sqlite:////path.sqlite3',
+    help='SQLite databases location',
 )
 @click.option(
-    '--db-uri',
+    '--sqlite-uri',
+    multiple=True,
+    help='SQLite database URI (e.g. sqlite:////path.sqlite3)',
+)
+@click.option(
+    '--pg-uri',
     multiple=True,
     help='Postgres connection URI (e.g. postgresql://user:pass@host)',
 )
-def main(db_dir, db_uri):
-    if len(db_dir) + len(db_uri) != 2:
-        raise click.BadParameter('exactly 2 db_dir or db_uri must be given')
+def main(db_dir, sqlite_uri, pg_uri):
+    if len(db_dir) + len(sqlite_uri) + len(pg_uri) != 2:
+        raise click.BadParameter('exactly 2 db_dir or sqlite_uri or pg_uri must be given')
     db_infos = []
     for db_dir_ in db_dir:
         db_infos.append(SqliteDatabaseInfo(db_dir_))
-    for db_uri_ in db_uri:
-        db_infos.append(PostgresDatabaseInfo(db_uri_))
-    differences = compare_databases(*db_infos)
+    for sqlite_uri_ in sqlite_uri:
+        db_infos.append(SqliteDatabaseInfo(sqlite_uri_))
+    for pg_uri_ in pg_uri:
+        db_infos.append(PostgresDatabaseInfo(pg_uri_))
+    exact = not (sqlite_uri and pg_uri)
+    differences = compare_databases(*db_infos, exact=exact)
     if differences:
         click.echo(f'Databases {db_infos[0]} and {db_infos[1]} are different:')
         for line in differences:
