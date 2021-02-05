5f6b9512644f0fa8a514e06dc17d06153ea5a21a
Jason Parham <bluemellophone@gmail.com>
2021-02-04 23:51:29 -0800

    Add squash commands for Docker

--- devops/build.sh
+++ devops/build.sh
@@ -13,13 +13,13 @@ cd ${ROOT_LOC}
 # Build the images in dependence order
 while [ $# -ge 1 ]; do
     if [ "$1" == "wbia-base" ]; then
-        docker build -t wildme/wbia-base:latest base
+        docker build --squash -t wildme/wbia-base:latest base
     elif [ "$1" == "wbia-dependencies" ]; then
-        docker build -t wildme/wbia-dependencies:latest dependencies
+        docker build --squash -t wildme/wbia-dependencies:latest dependencies
     elif [ "$1" == "wbia-provision" ]; then
-        docker build -t wildme/wbia-provision:latest provision
+        docker build --squash -t wildme/wbia-provision:latest provision
     elif [ "$1" == "wbia" ]; then
-        docker build --no-cache -t wildme/wbia:latest .
+        docker build --squash --no-cache -t wildme/wbia:latest .
     elif [ "$1" == "wildbook-ia" ]; then
         cd ../
         # Build the runtime container
