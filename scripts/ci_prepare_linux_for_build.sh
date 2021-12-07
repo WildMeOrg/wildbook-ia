9a857390f6157762228423a88813d42efb50093d
Jason Parham <bluemellophone@gmail.com>
2021-12-06 21:05:25 -0800

    Updates for actions

--- scripts/ci_prepare_linux_for_build.sh
+++ scripts/ci_prepare_linux_for_build.sh
@@ -5,3 +5,12 @@ set -ex
 export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pip install -r requirements/build.txt
+
+if command -v yum &> /dev/null
+then
+	echo "No-op"
+else
+    apt-get install \
+        pgloader \
+        graphviz
+fi
