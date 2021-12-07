e8d0897b175b339c18e5293577d4d34494ac595f
Jason Parham <bluemellophone@gmail.com>
2021-12-06 16:41:21 -0800

    Updates

---
+++ scripts/ci_prepare_macos_for_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PROJ_DIR=$(brew --prefix proj)
+
+brew install \
+    geos \
+    gdal \
+    proj \
+    graphviz
+
+pip install --global-option=build_ext --global-option="-I$(echo $(brew --prefix graphviz)/include/)" --global-option="-L$(echo $(brew --prefix graphviz)/lib/)" pygraphviz
+
+pip install -r requirements.txt
