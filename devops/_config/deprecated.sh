a2e43d77551a8303025db997d90b625769d5ff18
Jason Parham <parham@wildme.org>
2021-12-13 12:59:37 -0800

    Keep Numpy version current after tensorflow 1.15 install for PIE V1

--- devops/_config/deprecated.sh
+++ devops/_config/deprecated.sh
@@ -15,4 +15,7 @@ git clone https://github.com/WildMeOrg/wbia-plugin-pie.git
     'tensorflow-gpu==1.15.5' \
     'keras==2.2.5'
 
+/virtualenv/env3/bin/pip install --upgrade \
+    numpy
+
 /virtualenv/env3/bin/python -c "import wbia_pie; from wbia_pie.__main__ import main; main()"
