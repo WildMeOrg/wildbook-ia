cb49c59a448b0e6d2aa4f0ecb095bfb4308af954
Jason Parham <bluemellophone@gmail.com>
2022-05-12 16:19:02 -0700

    Allow WBIA to store multiple Houston token sessions based on the hostname

--- wbia/utils.py
+++ wbia/utils.py
@@ -14,51 +14,62 @@ HOUSTON_TOKEN_API = '%s/api/v1/auth/tokens'
 HOUSTON_CLIENT_ID = os.getenv('HOUSTON_CLIENT_ID')
 HOUSTON_CLIENT_SECRET = os.getenv('HOUSTON_CLIENT_SECRET')
 
-HOUSTON_SESSION = None
+HOUSTON_SESSIONS = {}
 
 
-def init_houston_session():
-    global HOUSTON_SESSION
+def init_houston_session(hostname):
+    global HOUSTON_SESSIONS
 
-    if HOUSTON_SESSION is None:
+    if HOUSTON_SESSIONS.get(hostname, None) is None:
         client = BackendApplicationClient(client_id=HOUSTON_CLIENT_ID)
-        HOUSTON_SESSION = OAuth2Session(client=client)
+        HOUSTON_SESSIONS[hostname] = OAuth2Session(client=client)
 
-    return HOUSTON_SESSION
+    return HOUSTON_SESSIONS[hostname]
+
+
+def forget_houston_session(hostname):
+    global HOUSTON_SESSIONS
+
+    return HOUSTON_SESSIONS.pop(hostname, None)
 
 
 def refresh_houston_session_token(hostname):
-    global HOUSTON_SESSION
+    session = init_houston_session(hostname)
 
-    HOUSTON_SESSION = init_houston_session()
-
-    HOUSTON_SESSION.fetch_token(
+    session.fetch_token(
         token_url=HOUSTON_TOKEN_API % (hostname,),
         client_id=HOUSTON_CLIENT_ID,
         client_secret=HOUSTON_CLIENT_SECRET,
     )
 
-    return HOUSTON_SESSION
+    return session
+
+
+def get_houston_session(hostname):
+    session = HOUSTON_SESSIONS.get(hostname, None)
+
+    if session is None:
+        session = refresh_houston_session_token(hostname)
+
+    return session
 
 
 def call_houston(uri, method='GET', retry=True, **kwargs):
-    global HOUSTON_SESSION
-
     clean_uri = uri.replace('houston+', '')
 
-    if HOUSTON_SESSION is None:
-        parse_uri = urlparse(clean_uri)
-        hostname = '%s://%s' % (
-            parse_uri.scheme,
-            parse_uri.netloc,
-        )
+    parse_uri = urlparse(clean_uri)
+    hostname = '%s://%s' % (
+        parse_uri.scheme,
+        parse_uri.netloc,
+    )
 
-        HOUSTON_SESSION = refresh_houston_session_token(hostname)
+    session = get_houston_session(hostname)
 
     try:
-        return HOUSTON_SESSION.request(method, clean_uri, **kwargs)
+        return session.request(method, clean_uri, **kwargs)
     except (Exception, TokenExpiredError):
         if retry:
+            forget_houston_session(hostname)
             return call_houston(uri, method=method, retry=False, **kwargs)
         else:
             raise
