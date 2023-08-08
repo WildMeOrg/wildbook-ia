2d97b46686c5c7e2ff39396949a6bb34d7a8e2f9
tsubramanian <tamilselvan@gmail.com>
2023-08-08 21:38:24 +0530

    Update run_developer_setup.sh

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -6,6 +6,9 @@ python setup.py clean
 pip install --no-cache-dir -r requirements.txt
 # pip install -r requirements.txt --use-deprecated=legacy-resolver
 
+pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
+pip install https://github.com/Lasagne/Lasagne/archive/master.zip
+
 python setup.py develop
 
 pip install --no-cache-dir -e .
