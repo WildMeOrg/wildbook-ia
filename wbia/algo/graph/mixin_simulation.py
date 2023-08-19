023b1521260e46dad1341269fe188385830a5303
tsubramanian <tamilselvan@gmail.com>
2023-08-19 12:45:28 +0530

    Update mixin_simulation.py

--- wbia/algo/graph/mixin_simulation.py
+++ wbia/algo/graph/mixin_simulation.py
@@ -302,7 +302,7 @@ class SimulationHelpers(object):
 
         test_print = ut.partial(infr.print, level=2)
 
-        #def test_print(x, **kw):
+        # def test_print(x, **kw):
         #    infr.print('[ACTION] ' + x, level=2, **kw)
 
         # test_print = lambda *a, **kw: None  # NOQA
