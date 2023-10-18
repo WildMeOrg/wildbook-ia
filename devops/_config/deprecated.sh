ef86a817ab9d04827b8d4d6214c047a4be5ee913
tsubramanian <tamilselvan@gmail.com>
2023-10-18 22:27:30 +0530

    Update deprecated.sh

--- devops/_config/deprecated.sh
+++ devops/_config/deprecated.sh
@@ -7,23 +7,26 @@ export TMPDIR=/home/$USER/tmp
 mkdir $TMPDIR
 export PIP_CACHE_DIR=/home/$USER/tmp
 /virtualenv/env3/bin/pip cache dir
-du -h
+df -h
 cd /wbia \
 
-git clone https://github.com/WildMeOrg/wbia-plugin-pie.git -b patch-1
+git clone https://github.com/tsubramanian/wbia-plugin-pie.git -b patch-1
 
 /virtualenv/env3/bin/pip install --cache-dir $TMPDIR --upgrade Cython
 /virtualenv/env3/bin/pip install --cache-dir $TMPDIR --upgrade gast
 
 /virtualenv/env3/bin/pip cache purge
-du -h
+df -h
+
 /bin/bash -xc '. /virtualenv/env3/bin/activate \
  && cd /wbia/wbia-plugin-pie \
  && /bin/bash run_developer_setup.sh'
-du -h
-# /virtualenv/env3/bin/pip install --cache-dir $TMPDIR \
-#    'tensorflow-gpu==1.15.5' \
-#    'keras==2.2.5'
+ 
+df -h
+
+/virtualenv/env3/bin/pip install --cache-dir $TMPDIR \
+    'tensorflow-gpu==1.15.5' \
+    'keras==2.2.5'
 
 /virtualenv/env3/bin/pip install --cache-dir $TMPDIR --upgrade \
     numpy
