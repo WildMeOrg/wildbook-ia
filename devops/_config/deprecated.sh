820ccaa6059a7dc8637d3c9c65dc524f3823693a
tsubramanian <tamilselvan@gmail.com>
2023-10-18 19:02:38 +0530

    Update deprecated.sh

--- devops/_config/deprecated.sh
+++ devops/_config/deprecated.sh
@@ -5,6 +5,9 @@ set -ex
 
 export TMPDIR=/home/$USER/tmp
 mkdir $TMPDIR
+export PIP_CACHE_DIR=/home/$USER/tmp
+/virtualenv/env3/bin/pip cache dir
+du -h
 cd /wbia \
 
 git clone https://github.com/WildMeOrg/wbia-plugin-pie.git
@@ -13,11 +16,11 @@ git clone https://github.com/WildMeOrg/wbia-plugin-pie.git
 /virtualenv/env3/bin/pip install --cache-dir $TMPDIR --upgrade gast
 
 /virtualenv/env3/bin/pip cache purge
-
+du -h
 /bin/bash -xc '. /virtualenv/env3/bin/activate \
  && cd /wbia/wbia-plugin-pie \
  && /bin/bash run_developer_setup.sh'
-
+du -h
 # /virtualenv/env3/bin/pip install --cache-dir $TMPDIR \
 #    'tensorflow-gpu==1.15.5' \
 #    'keras==2.2.5'
