9b6b580db45e6f90f2d007f846f49c6cf904d650
tsubramanian <tamilselvan@gmail.com>
2024-02-06 21:17:38 -0500

    Update deprecated.sh

--- devops/_config/deprecated.sh
+++ devops/_config/deprecated.sh
@@ -20,7 +20,7 @@ git clone https://github.com/WildMeOrg/wbia-plugin-pie.git
     'tensorflow-gpu==1.15.5' \
     'keras==2.2.5'
 
-/virtualenv/env3/bin/pip install ---no-cache-dir --upgrade \
+/virtualenv/env3/bin/pip install --no-cache-dir --upgrade \
     numpy
 
 /virtualenv/env3/bin/python -c "import wbia_pie; from wbia_pie.__main__ import main; main()"
